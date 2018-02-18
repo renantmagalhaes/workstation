4388914222fda4fa99a24e17c34ca492bd947286
Renan Toesqui Magalhães <renantmagalhaes@gmail.com>
2018-02-18 11:37:17 -0300

    Update workstation_installer.sh

--- workstation_installer.sh
+++ workstation_installer.sh
@@ -99,8 +99,8 @@ wget https://github.com/LinxGem33/OSX-Arc-Darker/releases/download/v1.4.5/osx-ar
 dpkg -i /tmp/osx-arc-collection_1.4.5_amd64.deb
 
 #Install Sublime Text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 apt-get update
 apt-get install sublime-text
 
