1cc4f4ce09e5f215844ce5544adab8b3ba1b0ac6
Renan Toesqui Magalhães <renantmagalhaes@gmail.com>
2018-05-26 19:28:47 -0300

    Update debian-workstation_installer.sh

--- debian-workstation_installer.sh
+++ debian-workstation_installer.sh
@@ -85,7 +85,7 @@ sed -e 's/main/main non-free/g' -i /etc/apt/sources.list
 apt-get update && apt-get upgrade
 
 #Install the packages from debian repo
-apt-get -y install docky clementine deluge dia vim vim-gtk vim-gui-common nmap vlc gimp blender fonts-powerline inkscape brasero gparted wireshark tmux curl net-tools iproute2 vpnc-scripts network-manager-vpnc vpnc network-manager-vpnc-gnome x2goclient thunar thunar-archive-plugin thunar-data thunar-dbg thunar-media-tags-plugin thunar-volman xfce4-goodies xfce4-*plugin git gnome-icon-theme idle3 mate-sensors-applet numix-gtk-theme numix-icon-theme firmware-linux firmware-linux-nonfree firmware-linux-free fonts-hack-ttf apt-transport-https htop python3-pip meld gnome-shell-ex*
+apt-get -y install docky clementine deluge dia vim vim-gtk vim-gui-common nmap vlc gimp blender fonts-powerline inkscape brasero gparted wireshark tmux curl net-tools iproute2 vpnc-scripts network-manager-vpnc vpnc network-manager-vpnc-gnome x2goclient thunar thunar-archive-plugin thunar-data thunar-dbg thunar-media-tags-plugin thunar-volman xfce4-goodies xfce4-*plugin git gnome-icon-theme idle3 mate-sensors-applet numix-gtk-theme numix-icon-theme firmware-linux firmware-linux-nonfree firmware-linux-free fonts-hack-ttf apt-transport-https htop python3-pip meld gnome-shell-ex* chrome-gnome-shell
 
 
 ####### Testing google-chrome for now ######
