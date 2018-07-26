f7eacc90d89b2ded027a2c7c6c4eb396c28b751f
Renan Toesqui Magalhães <renantmagalhaes@gmail.com>
2018-07-26 15:14:07 -0300

    v 0.7

--- desktop/debian-workstation_installer.sh
+++ desktop/debian-workstation_installer.sh
@@ -64,6 +64,7 @@
 #       - Add gnome-terminal package
 #       - Add Gogh -Color Scheme for Gnome Terminal and Pantheon Terminal (https://github.com/Mayccoll/Gogh)
 #       - Changed Thunar > Caja
+#       - Changed OMF for Fisherman
 #
 #
 #
@@ -167,12 +168,10 @@ wget --content-disposition https://go.microsoft.com/fwlink/?LinkID=760868 -O /tm
 dpkg -i /tmp/visual_code_amd64.deb
 
 #Add Gogh
-## Grubvbox dark
-wget -O xt https://git.io/v7eBS && chmod +x xt && ./xt && rm xt
 ##Elementary
-wget -O xt  http://git.io/v3D8R && chmod +x xt && ./xt && rm xt
-
-
+runuser -l $user -c 'wget -O xt  http://git.io/v3D8R && chmod +x xt && ./xt && rm xt'
+## Grubvbox dark
+runuser -l $user -c 'wget -O xt https://git.io/v7eBS && chmod +x xt && ./xt && rm xt'
 
 #Install fish
 wget -nv https://download.opensuse.org/repositories/shells:fish:release:2/Debian_9.0/Release.key -O Release.key
@@ -221,10 +220,22 @@ EOF
 chown $user:$user /home/$user/.vimrc
 chown $user:$user /home/$user/.gvimrc
 
-#Set Oh My Fish
-#Credits <https://github.com/oh-my-fish/oh-my-fish>
-runuser -l $user -c 'curl -L https://get.oh-my.fish > install'
-runuser -l $user -c 'fish install --path=~/.local/share/omf --config=~/.config/omf --noninteractive'
+##Set Oh My Fish
+##Credits <https://github.com/oh-my-fish/oh-my-fish>
+#runuser -l $user -c 'curl -L https://get.oh-my.fish > install'
+#runuser -l $user -c 'fish install --path=~/.local/share/omf --config=~/.config/omf --noninteractive'
+
+
+#Set Fisherman(https://github.com/fisherman/fisherman)
+runuser -l $user -c 'curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher'
+
+#Add Fisherman packages
+runuser -l $user -c 'fisher install edc/bass'
+runuser -l $user -c 'fisher install laughedelic/pisces'
+runuser -l $user -c 'fisher install z'
+runuser -l $user -c 'fisher install omf/theme-bobthefish'
+
+#Add fish config
 runuser -l $user -c 'touch ~/.config/fish/config.fish'
 
 cat <<EOF >> /home/$user/.config/fish/config.fish
