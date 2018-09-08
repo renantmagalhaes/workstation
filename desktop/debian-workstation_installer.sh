0fb85c0c64b2fac69bc3e1063467eb804a850ef0
Rentoma <renantmagalhaes@github.com>
2018-09-08 14:49:41 -0300

    syntax adjustment

--- desktop/debian-workstation_installer.sh
+++ desktop/debian-workstation_installer.sh
@@ -296,7 +296,8 @@ set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
 set -g default_user your_normal_user
-set -g theme_color_scheme dark
+#set -g theme_color_scheme dark
+set -g theme_color_scheme zenburn
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 1
 EOF
