eb06bc7ec8c5fb4ec1aab262cc5fa4c7350366bc
renantmagalhaes <renantmagalhaes@gmail.com>
2020-01-22 18:16:29 +0100

    dbeaver

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -21,4 +21,4 @@ sudo dpkg -i /tmp/XMind-ZEN-for-Linux-64bit.deb
 # DBeaver
 wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
 echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
-sudo apt-get update && sudo apt-get install dbeaver-ce
+sudo apt-get update && sudo apt-get install -y dbeaver-ce
