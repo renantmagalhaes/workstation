683e54ded9573a48751cf386fc7c7b553c239cf7
renantmagalhaes <renantmagalhaes@gmail.com>
2020-01-22 18:15:00 +0100

    dbeaver

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -18,3 +18,7 @@ sudo snap install freemind
 wget --user-agent="Mozilla/4.0 (Windows; MSIE 7.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)" www.xmind.net/xmind/downloads/XMind-ZEN-for-Linux-64bit.deb -O /tmp/XMind-ZEN-for-Linux-64bit.deb
 sudo dpkg -i /tmp/XMind-ZEN-for-Linux-64bit.deb
 
+# DBeaver
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt-get update && sudo apt-get install dbeaver-ce
