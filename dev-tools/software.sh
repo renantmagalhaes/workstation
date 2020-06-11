c7fb740abe63436b91200b87a283b2bc8ffba867
renantmagalhaes <renantmagalhaes@gmail.com>
2020-06-11 21:51:52 +0200

    docker suite

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -8,7 +8,7 @@ sudo snap install redis-desktop-manager
 sudo snap install robo3t-snap
 
 #Mysql Workbench
-sudo apt install -y mysql-workbench
+sudo snap install mysql-workbench-community --candidate
 
 #Mindmap
 ## Freemind
@@ -22,3 +22,13 @@ sudo dpkg -i /tmp/XMind-ZEN-for-Linux-64bit.deb
 wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
 echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
 sudo apt-get update && sudo apt-get install -y dbeaver-ce
+
+# Install Docker
+## Install latest docker
+curl -sSL https://get.docker.com/ | sh
+sudo usermod -aG docker $USER
+
+
+#Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
