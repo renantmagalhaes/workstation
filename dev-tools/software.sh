4dfbbdbc4ea363cb8295dba9ed71ad275c67ba64
Renan Toesqui Magalhaes <rtm@pop-os.localdomain>
2020-08-20 21:39:56 +0200

    update_software

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -32,6 +32,10 @@ curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker $USER
 
 
+## Install Docker Repo Version
+sudo apt install docker docker.io
+sudo usermod -aG docker `whoami`
+
 #Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
