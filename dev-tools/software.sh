1dfa09e5763c2de8b72cc2b3be1f76180de472fe
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2020-08-22 18:42:10 +0200

    docker for fedora

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -31,10 +31,28 @@ sudo apt-get update && sudo apt-get install -y dbeaver-ce
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker $USER
 
-
-## Install Docker Repo Version
+## Install Docker Repo Version - Ubuntu
 sudo apt install docker docker.io
 sudo usermod -aG docker `whoami`
+
+## Install Docker Repo Version - Fedora 32
+sudo dnf -y install dnf-plugins-core
+
+sudo tee /etc/yum.repos.d/docker-ce.repo<<EOF
+[docker-ce-stable]
+name=Docker CE Stable - \$basearch
+baseurl=https://download.docker.com/linux/fedora/31/\$basearch/stable
+enabled=1
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/fedora/gpg
+EOF
+sudo dnf makecache
+sudo dnf install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable --now docker
+sudo usermod -aG docker $(whoami)
+newgrp docker
+sudo mkdir /sys/fs/cgroup/systemd
+sudo mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
 
 #Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
