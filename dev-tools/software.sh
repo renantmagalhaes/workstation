bfe3cb6d7f338db7d1b40583053cf7840f536973
renantmagalhaes <renantmagalhaes@gmail.com>
2020-08-25 09:46:11 +0200

    docker for fedora

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -52,6 +52,11 @@ sudo usermod -aG docker $(whoami)
 newgrp docker
 sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
 
+sudo firewall-cmd --zone=FedoraWorkstation --add-masquerade --permanent
+sudo firewall-cmd --reload
+sudo systemctl restart docker
+
+
 #Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
