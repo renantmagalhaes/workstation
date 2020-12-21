74e82c5934c67b2667b16e6ef6302729a60cefd3
rtm <renantmagalhaes@gmail.com>
2020-12-21 22:11:49 +0100

    gtk update

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -40,7 +40,18 @@ sudo curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/y
 sudo dnf update -y
 sudo dnf install -y yarn nodejs
 
-# Install Docker Repo Version - Fedora 33
+## DEB based
+sudo apt install -y curl gnupg2 gcc-c++ make
+sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update -y
+sudo apt install -y yarn nodejs
+
+# Docker
+
+## Fedora
+###Install Docker Repo Version - Fedora 33 (Maybe not needed anymore after cgroups update)
 sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
 sudo firewall-cmd --permanent --zone=trusted --add-interface=docker0
 sudo firewall-cmd --permanent --zone=FedoraWorkstation --add-masquerade
@@ -50,3 +61,13 @@ sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl restart docker
+
+## DEB Based 20.04
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl enable docker
+sudo systemctl restart docker
+
