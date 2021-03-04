6c32c3a4899da09e41e93872f5149d66d5eac5cd
renantmagalhaes <renantmagalhaes@gmail.com>
2021-03-04 16:51:32 +0100

    docker-compose pkg

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -65,7 +65,7 @@ sudo apt install -y yarn nodejs
 #sudo systemctl restart docker
 #
 ## DEB Based 20.04
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common docker-compose
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
