84a1cc29a1ca3b74eac591c7984fd02a20204547
renantmagalhaes <renantmagalhaes@gmail.com>
2021-03-13 00:57:34 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#Root check
-#if [ “$(id -u)” != “0” ]; then
-#clear
-#echo -e “This script must be run as root '\n' sudo ./install.sh” 2>&1
-#exit 1
-#fi
+# Root Verification 
+if [ “$(id -u)” = “0” ]; then
+echo “Dont run this script as root” 2>&1
+exit 1
+fi
 
 
 echo "#### Menu Selector ####"
@@ -13,8 +12,8 @@ echo " 1) Install Main SO packages"
 echo " 2) Install Tmux"
 echo " 3) Install Oh my ZSH"
 echo " 4) Install ZSH additional packages"
-echo " 5) Install DEV packages"
-echo " 6) Guake Settings"
+echo " 5) Guake Settings"
+echo " 6) Install DEV Tools"
 echo " 9)Exit"
 
 read n
@@ -23,8 +22,8 @@ case $n in
     2) bash tmux/tmux.sh ;;
     3) bash zsh/zsh.sh ;;
     4) bash zsh/zsh.sh ;;
-    5) bash dev-tools/software.sh ;;
-    6) guake --restore-preferences guake/rtm-guake-settings ;;
+    5) guake --restore-preferences guake/rtm-guake-settings ;;    
+    6) bash dev-tools/software.sh ;;
     9)  echo "Bye" || exit ;;
   *) echo "Invalid option";;
 esac
