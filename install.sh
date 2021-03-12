a6c53161fa8d9945a10b512f2dc2a963de3b8fa2
renantmagalhaes <renantmagalhaes@gmail.com>
2021-03-13 00:54:22 +0100

    ZSH additional packages

--- install.sh
+++ install.sh
@@ -11,9 +11,10 @@
 echo "#### Menu Selector ####"
 echo " 1) Install Main SO packages"
 echo " 2) Install Tmux"
-echo " 3) Install ZSH"
-echo " 4) Install DEV packages"
-echo " 5) Guake Settings"
+echo " 3) Install Oh my ZSH"
+echo " 3) Install ZSH additional packages"
+echo " 5) Install DEV packages"
+echo " 6) Guake Settings"
 echo " 9)Exit"
 
 read n
@@ -21,8 +22,9 @@ case $n in
     1) bash desktop/0-DEB/0-Pop\!_OS.sh ;;
     2) bash tmux/tmux.sh ;;
     3) bash zsh/zsh.sh ;;
-    4) bash dev-tools/software.sh ;;
-    5) guake --restore-preferences guake/rtm-guake-settings ;;
+    4) bash zsh/zsh.sh ;;
+    5) bash dev-tools/software.sh ;;
+    6) guake --restore-preferences guake/rtm-guake-settings ;;
     9)  echo "Bye" || exit ;;
   *) echo "Invalid option";;
 esac
