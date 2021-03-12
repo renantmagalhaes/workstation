e93d423d179eed4ea6f0d95764e0960d627c50a1
renantmagalhaes <renantmagalhaes@gmail.com>
2021-03-13 00:51:47 +0100

    install.sh path fix

--- vim/VIm_script.sh
+++ vim/VIm_script.sh
@@ -2,3 +2,5 @@
 curl -sLf https://spacevim.org/install.sh | bash
 echo "set ignorecase" >> ~/.vim/vimrc
 echo "set paste" >> ~/.vim/vimrc
+echo "set cryptmethod=blowfish2" >> ~/.vim/vimrc
+echo "set viminfo=" >> ~/.vim/vimrc
