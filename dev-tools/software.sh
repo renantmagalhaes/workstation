f6c8a21486e61ad624a68a219098b6a2c1c83480
renantmagalhaes <renantmagalhaes@gmail.com>
2021-04-17 12:41:03 +0200

    docker-compose fix

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -65,11 +65,11 @@ sudo apt install -y yarn nodejs
 #sudo systemctl restart docker
 #
 ## DEB Based 20.04
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common docker-compose
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install -y docker-ce
+sudo apt install -y docker-ce docker-compose
 sudo systemctl enable docker
 sudo systemctl restart docker
 sudo usermod -aG docker $USER
