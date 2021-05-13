31ed925a98d5ab654c78b0ef63d882ba3ef54442
renantmagalhaes <renantmagalhaes@gmail.com>
2021-05-13 14:59:37 +0200

    pgAdmin4 pkg

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -34,6 +34,12 @@ sudo flatpak install -y flathub net.sourceforge.jpdftweak.jPdfTweak
 # K8S IDE - Lens
 sudo snap install kontena-lens --classic
 
+# pgAdmin
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install -y pgadmin4-desktop
+
+
 #Yarn and NodeJs
 ### Fedora
 #sudo dnf install -y curl gnupg2 gcc-c++ make
