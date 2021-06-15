110dfeddc5d925aedd77e43e86635f65b8503daf
renantmagalhaes <renantmagalhaes@gmail.com>
2021-06-15 12:35:05 +0200

    disable G810 pkg

--- cron/cron-scripts.sh
+++ cron/cron-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## G810 color profileG910 
-(crontab -l 2>/dev/null; echo "@reboot g810-led -p /usr/share/doc/g810-led/examples/sample_profiles/colors") | crontab -
+# (crontab -l 2>/dev/null; echo "@reboot g810-led -p /usr/share/doc/g810-led/examples/sample_profiles/colors") | crontab -
 
 ## Caffeine stop
 (crontab -l 2>/dev/null; echo "0 * * * * $PWD/jobs/caffeine-stop.sh") | crontab -
