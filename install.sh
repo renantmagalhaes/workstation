b3f6c531106dd7ebd1f60620ed2be353fb168530
renantmagalhaes <renantmagalhaes@gmail.com>
2021-07-16 13:01:30 +0200

    os-selector.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ echo " 0)Exit"
 
 read n
 case $n in
-    1) bash desktop/0-DEB/0-system.sh ;;
+    1) bash utils/os-selector/os-selector.sh ;;
     2) bash tmux/tmux.sh ;;
     3) bash zsh/zsh.sh ;;
     4) bash zsh/zsh.sh ;;
