09edb7e14c286d65b065b886f8b3ca5d33268d31
renantmagalhaes <renantmagalhaes@gmail.com>
2021-07-17 11:11:21 +0200

    kubuntu.sh

--- desktop/old/deb/Kubuntu.sh
+++ desktop/old/deb/Kubuntu.sh
@@ -13,12 +13,12 @@
 #*  -> Preferred applications
 #*      - Web: Vivaldi / Google Chrome
 #*      - Editor: Visual Studio Code
-#*      - Music: Clementine / Spotify(web)
+#*      - Music: Clementine / YT Music(web)
 #*      - Video: VLC 
 #*      - Terminal: Guake 
-#*      - File Manager: Nautilus
+#*      - File Manager: Dolphin
 #*      - Record Desktop: OBS Studio
-#*      - Screenshot tool: Default Gnome / Flameshot
+#*      - Screenshot tool: Flameshot
 #//     - Mail: Thunderbird
 #
 #  --------------------------------------------------------------
@@ -28,19 +28,28 @@
 #    V0.1 2020-12-29 RTM:
 #        - Initial release
 #
+#    V0.1 2021-07-17 RTM:
+#        - Update latest packages
+#
+#
 #
 #    TODO
 # RTM
 
-#Root check
+
+# RTM
+
+# Verifications 
 if [ “$(id -u)” = “0” ]; then
 echo “Dont run this script as root” 2>&1
 exit 1
 fi
 
-# Add keys and ppas
+# Add keys, ppa and repos
 ## VirtualBox
 #wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+## Flat-Remix Theme
+#sudo add-apt-repository ppa:daniruiz/flat-remix
 ## Vivaldi Browser
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
 sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
@@ -49,35 +58,49 @@ sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
 sudo apt-get update && sudo apt-get -y upgrade
 
 # Install the packages from repo
-sudo apt-get -y install latte-dock plank zsh clementine breeze-cursor-theme vim vim-gtk vim-gui-common nmap vlc blender fonts-powerline brasero gparted wireshark tmux curl net-tools iproute2 x2goclient git idle3 fonts-hack-ttf apt-transport-https htop meld dconf-cli openvpn snapd guake guake-indicator krita frei0r-plugins audacity filezilla tree remmina remmina-plugin-rdp ffmpeg nload virtualbox flatpak pwgen sysstat alacarte fzf ffmpeg neofetch xclip flameshot unrar python3-pip bat gawk net-tools coreutils gir1.2-gtop-2.0 lm-sensors obs-studio kdenlive
+sudo apt-get -y install latte-dock zsh clementine breeze-cursor-theme vim vim-gui-common nmap vlc blender fonts-powerline brasero gparted wireshark tmux curl net-tools iproute2 x2goclient git idle3 fonts-hack-ttf apt-transport-https htop meld dconf-cli openvpn snapd guake guake-indicator krita frei0r-plugins audacity filezilla tree remmina remmina-plugin-rdp ffmpeg nload virtualbox flatpak pwgen sysstat alacarte fzf ffmpeg neofetch xclip flameshot unrar python3-pip bat gawk net-tools coreutils gir1.2-gtop-2.0  obs-studio nordvpn cheese ncdu whois pdfshuffler piper libratbag-tools
+sudo apt-get -f install -y
+
+
+# vboxuser
+sudo usermod -aG vboxusers $USER
 
 # Flatpack repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Flathub Packages 
+#Utils
+
+## Fix snapd
+sudo ln -s /var/lib/snapd/snap /snap
+
+## Skype
 sudo flatpak install -y flathub com.skype.Client
 
 ## Zoom
 sudo flatpak install -y flathub us.zoom.Zoom
 
-# Install Handbrake - Video Converter
+## Install Handbrake - Video Converter
 sudo flatpak install -y flathub fr.handbrake.ghbE
 
-# Slack
+## Microsoft teams
+sudo flatpak install -y flathub com.microsoft.Teams
+
+## FFaudioConverter
+sudo flatpak install -y flathub com.github.Bleuzen.FFaudioConverter
+
+## Kdenlive
+sudo flatpak install flathub -y org.kde.kdenlive
+
+## MkCron
+sudo snap install mkcron
+
+## Slack
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.12.2-amd64.deb -O /tmp/slack-desktop.deb
 sudo dpkg -i /tmp/slack-desktop.deb
 
-# Microsoft teams
-wget https://go.microsoft.com/fwlink/p/?LinkID=2112886&clcid=0x409&culture=en-us&country=US -O /tmp/microsoft-teams.deb
-sudo dpkg -i /tmp/microsoft-teams.deb
-
-#Utils
 ## LSD
 wget https://github.com/Peltoche/lsd/releases/download/0.19.0/lsd_0.19.0_amd64.deb -O /tmp/lsd_amd64.deb
 sudo dpkg -i /tmp/lsd_amd64.deb
-
-## Fix snapd
-sudo ln -s /var/lib/snapd/snap /snap
 
 ## Fix python default path
 sudo ln -s /usr/bin/python3 /usr/bin/python
@@ -86,10 +109,26 @@ sudo ln -s /usr/bin/pip3 /usr/bin/pip
 ## Teamviewer
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb -O /tmp/teamviewer_amd64.deb
 sudo dpkg -i /tmp/teamviewer_amd64.deb
-sudo apt install -f -y
+sudo apt-get -f install -y
+
+## Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+sudo apt-get -f install -y
+
+## Install Vivaldi Browser
+sudo apt install -y vivaldi-stable
+
+## Install Visual Code
+wget --content-disposition https://go.microsoft.com/fwlink/?LinkID=760868 -O /tmp/visual_code_amd64.deb
+sudo dpkg -i /tmp/visual_code_amd64.deb
+sudo sed -i 's/\,arm64\,armhf//g' /etc/apt/sources.list.d/vscode.list
+sudo apt-get -f install -y
+
 
 ## Guake Configs
 mkdir -p ~/.config/autostart/
+#guake --save-preferences ../../guake/rtm-guake-settings
 guake --restore-preferences ../../guake/rtm-guake-settings 
 cat <<EOF >> ~/.config/autostart/guake.desktop
 [Desktop Entry]
@@ -104,32 +143,27 @@ StartupNotify=true
 X-Desktop-File-Install-Version=0.22
 EOF
 
+# Enable BT FastConnectable
+sudo sed -i 's/\#FastConnectable\ =\ false/FastConnectable\ =\ true/' /etc/bluetooth/main.conf
 
 # Install pip packages
-sudo pip3 install virtualenv virtualenvwrapper
+sudo pip3 install virtualenv virtualenvwrapper pylint
 sudo pip3 install bpytop --upgrade
+sudo apt-get -f install -y
+
+#Isolate Alt-Tab workspaces
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+
+# New VIM
+sudo apt-get install -y build-essential
+curl -sLf https://spacevim.org/install.sh | bash
+echo "set ignorecase" >> ~/.vim/vimrc
+echo "set cryptmethod=blowfish2" >> ~/.vim/vimrc
+echo "set viminfo=" >> ~/.vim/vimrc
+sudo apt-get -f install -y
 
 # Create git-folder
 mkdir -p ~/GIT-REPOS/CORE
-
-# Install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
-sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
-
-# Install Vivaldi Browser
-sudo apt install -y vivaldi-stable
-
-## Install Visual Code
-wget --content-disposition https://go.microsoft.com/fwlink/?LinkID=760868 -O /tmp/visual_code_amd64.deb
-sudo dpkg -i /tmp/visual_code_amd64.deb
-sudo sed -i 's/\,arm64\,armhf//g' /etc/apt/sources.list.d/vscode.list
-
-## G810 color profile
-sudo apt install -y g810-led
-sudo g810-led -p /etc/g810-led/samples/colors
-#sudo g810-led -p /etc/g810-led/samples/group_keys
-## Set color scheme on boot
-(crontab -l 2>/dev/null; echo "@reboot g810-led -p /usr/share/doc/g810-led/examples/sample_profiles/colors") | crontab -
 
 # Install Fonts
 git clone https://github.com/powerline/fonts.git ~/GIT-REPOS/CORE/fonts/
@@ -152,17 +186,13 @@ unzip ~/.local/share/fonts/3270.zip -d ~/.local/share/fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip -O ~/.local/share/fonts/Agave.zip
 unzip ~/.local/share/fonts/Agave.zip -d ~/.local/share/fonts/
 
+## cascadia font for vscode
+wget https://github.com/microsoft/cascadia-code/releases/download/v2105.24/CascadiaCode-2105.24.zip -O /tmp/CascadiaCode-2105.24.zip
+unzip /tmp/CascadiaCode-2105.24.zip -d /tmp/
+cp /tmp/ttf/CascadiaCodePL.ttf  ~/.local/share/fonts/
+cp /tmp/ttf/CascadiaCode.ttf  ~/.local/share/fonts/
+
 fc-cache -vf ~/.local/share/fonts/
-
-# Kvantum
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt-get update
-sudo apt install -y qt5-style-kvantum 
-
-# New VIM
-sudo apt-get install -y build-essential
-curl -sLf https://spacevim.org/install.sh | bash
-echo "set ignorecase" >> ~/.vim/vimrc
 
 # Themes
 # Qogir
@@ -185,20 +215,28 @@ sh -c "~/GIT-REPOS/CORE/ChromeOS-kde/install.sh"
 git clone https://github.com/vinceliuice/WhiteSur-kde.git ~/GIT-REPOS/CORE/WhiteSur-kde
 sh -c "~/GIT-REPOS/CORE/WhiteSur-kde/install.sh"
 
-
-# Icons
 # Tela-circle-icon-theme
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git ~/GIT-REPOS/CORE/Tela-circle-icon-theme
 sh -c "~/GIT-REPOS/CORE/Tela-circle-icon-theme/install.sh -a"
 
+# Flatery Icon Theme
+git clone https://github.com/cbrnix/Flatery.git ~/GIT-REPOS/CORE/Flatery
+ln -s ~/GIT-REPOS/CORE/Flatery/Flatery ~/.local/share/icons/Flatery
+ln -s ~/GIT-REPOS/CORE/Flatery/Flatery-Indigo-Dark ~/.local/share/icons/Flatery-Indigo-Dark
+
 # Colorls
 sudo apt install -y ruby-dev
 sudo gem install colorls
+sudo apt-get -f install -y
 
-#Widgets 
-git clone https://github.com/wsdfhjxc/virtual-desktop-bar.git ~/GIT-REPOS/CORE/virtual-desktop-bar
-sh -c "~/GIT-REPOS/CORE/virtual-desktop-bar/scripts/install-dependencies-ubuntu.sh"
-sh -c "~/GIT-REPOS/CORE/virtual-desktop-bar/scripts/install-applet.sh"
+
+# Install ClamAV
+sudo apt install -y clamav clamtk
+sudo apt-get -f install -y
+sudo apt-get install -y clamav-daemon
+
+# Make sure all package are installed
+sudo apt-get -f install -y
 
 
 # RTM
