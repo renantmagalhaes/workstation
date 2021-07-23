dee19bdf96df3896e4f67836acb13af488f57ff4
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2021-07-23 13:12:22 +0200

    kvantum order

--- desktop/old/deb/Kubuntu.sh
+++ desktop/old/deb/Kubuntu.sh
@@ -53,11 +53,13 @@ fi
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
 sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
 
+## Kvantum
+sudo add-apt-repository ppa:papirus/papirus
 # Update / upgrade
 sudo apt-get update && sudo apt-get -y upgrade
 
 # Install the packages from repo
-sudo apt-get -y install latte-dock zsh clementine breeze-cursor-theme vim vim-gui-common nmap vlc blender fonts-powerline brasero gparted wireshark tmux curl net-tools iproute2 x2goclient git idle3 fonts-hack-ttf apt-transport-https htop meld dconf-cli openvpn snapd guake guake-indicator krita frei0r-plugins audacity filezilla tree remmina remmina-plugin-rdp ffmpeg nload virtualbox flatpak pwgen sysstat alacarte fzf ffmpeg neofetch xclip flameshot unrar python3-pip bat gawk net-tools coreutils gir1.2-gtop-2.0 obs-studio cheese ncdu whois pdfshuffler piper libratbag-tools
+sudo apt-get -y install latte-dock zsh clementine breeze-cursor-theme vim vim-gui-common nmap vlc blender fonts-powerline brasero gparted wireshark tmux curl net-tools iproute2 x2goclient git idle3 fonts-hack-ttf apt-transport-https htop meld dconf-cli openvpn snapd guake guake-indicator krita frei0r-plugins audacity filezilla tree remmina remmina-plugin-rdp ffmpeg nload virtualbox flatpak pwgen sysstat alacarte fzf ffmpeg neofetch xclip flameshot unrar python3-pip bat gawk net-tools coreutils gir1.2-gtop-2.0 obs-studio cheese ncdu whois pdfshuffler piper libratbag-tools qt5-style-kvantum
 sudo apt-get -f install -y
 
 
@@ -189,11 +191,6 @@ cp /tmp/ttf/CascadiaCodePL.ttf  ~/.local/share/fonts/
 cp /tmp/ttf/CascadiaCode.ttf  ~/.local/share/fonts/
 
 fc-cache -vf ~/.local/share/fonts/
-
-# Kvantum
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt-get update
-sudo apt install -y qt5-style-kvantum 
 
 # Themes
 # Qogir
