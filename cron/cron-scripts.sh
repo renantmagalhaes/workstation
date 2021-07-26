9b999585d3b11fc17c52d27690de1033c2cc0ae2
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2021-07-26 12:38:36 +0200

    cron envs

--- cron/cron-scripts.sh
+++ cron/cron-scripts.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+
+(crontab -l 2>/dev/null; echo "SHELL=/bin/sh") | crontab -
+(crontab -l 2>/dev/null; echo "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin") | crontab -
+
+
 ## G810 color profileG910 
 # (crontab -l 2>/dev/null; echo "@reboot g810-led -p /usr/share/doc/g810-led/examples/sample_profiles/colors") | crontab -
 
-## Caffeine stop
-(crontab -l 2>/dev/null; echo "0 * * * * $PWD/jobs/caffeine-stop.sh") | crontab -
+# ## Caffeine stop
+# (crontab -l 2>/dev/null; echo "0 * * * * $PWD/jobs/caffeine-stop.sh") | crontab -
 
-## Caffeine start
-(crontab -l 2>/dev/null; echo "@reboot $PWD/jobs/caffeine-start.sh") | crontab -
+# ## Caffeine start
+# (crontab -l 2>/dev/null; echo "@reboot $PWD/jobs/caffeine-start.sh") | crontab -
