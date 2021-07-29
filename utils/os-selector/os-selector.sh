273b45ffd12cab86cc39e178d68b954ccfdcd81b
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2021-07-29 21:44:20 +0200

    os-selector gnome and kde

--- utils/os-selector/os-selector.sh
+++ utils/os-selector/os-selector.sh
@@ -1,13 +1,21 @@
+#!/bin/bash
 # Select DEB or RPM system
+gnome_check=`env |grep XDG_CURRENT_DESKTOP |grep -ioh "GNOME" | awk '{print tolower($0)}'`
+kde_check=`env |grep XDG_CURRENT_DESKTOP |grep -ioh "KDE" | awk '{print tolower($0)}'`
 
 # check cmd function
 check_cmd() {
     command -v "$1" 2> /dev/null
 }
 
-
 if check_cmd apt-get; then # FOR DEB SYSTEMS
-    bash desktop/0-DEB/0-system.sh
+    if [[ $gnome_check == "gnome" ]]; then
+    bash desktop/0-DEB/0-gnome-system.sh
+    elif [[ $kde_check == "kde" ]]; then
+    bash desktop/0-DEB/0-kde-system.sh
+    else
+    echo "Not able to identify desktop environment"
+    fi
 elif check_cmd dnf; then  # FOR RPM SYSTEMS
      bash desktop/1-RPM/0-system.sh
 else
