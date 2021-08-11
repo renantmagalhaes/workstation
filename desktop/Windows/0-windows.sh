e35dcaccfdfd60715b396e95142d99f70f140187
Renan Toesqui Magalhães <renantmagalhaes@gmail.com>
2021-08-11 21:37:34 +0200

    Create 0-windows.sh

---
+++ desktop/Windows/0-windows.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+#
+#
+#?Site        :https://rtm.codes
+#?Author      :Renan Toesqui Magalhães <renantmagalhaes@gmail.com>
+#?                                     <https://github.com/renantmagalhaes>
+#
+# RTM
+
+# Verifications 
+if [ “$(id -u)” = “0” ]; then
+echo “Dont run this script as root” 2>&1
+exit 1
+fi
+
+
+# Update / upgrade
+sudo apt-get update && sudo apt-get -y upgrade
+
+# Install the packages from repo
+sudo apt-get -y install zsh fonts-powerline vim tmux curl net-tools iproute2 git fonts-hack-ttf apt-transport-https htop meld tree nload pwgen sysstat fzf neofetch xclip unrar unzip python3 python3-pip net-tools  ncdu whois 
+sudo apt-get -f install -y
+
+#Utils
+
+## LSD
+wget https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_amd64.deb -O /tmp/lsd_amd64.deb
+sudo dpkg -i /tmp/lsd_amd64.deb
+
+## Fix python default path
+sudo ln -s /usr/bin/python3 /usr/bin/python
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
+
+# Install pip packages
+sudo pip3 install virtualenv virtualenvwrapper pylint
+sudo pip3 install bpytop --upgrade
+sudo apt-get -f install -y
+
+# New VIM
+sudo apt-get install -y build-essential
+curl -sLf https://spacevim.org/install.sh | bash
+echo "set ignorecase" >> ~/.vim/vimrc
+echo "set cryptmethod=blowfish2" >> ~/.vim/vimrc
+echo "set viminfo=" >> ~/.vim/vimrc
+sudo apt-get -f install -y
+
+# Create git-folder 
+mkdir -p ~/GIT-REPOS/CORE
+
+# Install Fonts
+git clone https://github.com/powerline/fonts.git ~/GIT-REPOS/CORE/fonts/
+bash ~/GIT-REPOS/CORE/fonts/install.sh
+
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -O ~/.local/share/fonts/PowerlineSymbols.otf
+
+mkdir -p ~/.config/fontconfig/conf.d/
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -O ~/.config/fontconfig/conf.d/10-powerline-symbols.conf
+
+git clone https://github.com/gabrielelana/awesome-terminal-fonts.git ~/GIT-REPOS/CORE/awesome-terminal-fonts
+sh -c "~/GIT-REPOS/CORE/awesome-terminal-fonts/install.sh"
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O ~/.local/share/fonts/FiraCode.zip
+unzip ~/.local/share/fonts/FiraCode.zip -d ~/.local/share/fonts/
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip -O ~/.local/share/fonts/3270.zip
+unzip ~/.local/share/fonts/3270.zip -d ~/.local/share/fonts/
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip -O ~/.local/share/fonts/Agave.zip
+unzip ~/.local/share/fonts/Agave.zip -d ~/.local/share/fonts/
+
+## cascadia font for vscode
+wget https://github.com/microsoft/cascadia-code/releases/download/v2105.24/CascadiaCode-2105.24.zip -O /tmp/CascadiaCode-2105.24.zip
+unzip /tmp/CascadiaCode-2105.24.zip -d /tmp/
+cp /tmp/ttf/CascadiaCodePL.ttf  ~/.local/share/fonts/
+cp /tmp/ttf/CascadiaCode.ttf  ~/.local/share/fonts/
+
+# Colorls
+sudo apt install -y ruby-dev
+sudo gem install colorls
+sudo apt-get -f install -y
+
+# Make sure all package are installed
+sudo apt-get -f install -y
+
+# RTM
+# RTM
+#clear
+echo "#################################"
+echo "#                               #"
+echo "#         rtm.codes             #"
+echo "#     Please reboot your pc     #"
+echo "#                               #"
+echo "#################################"
