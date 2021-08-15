f8f0dffc6ab4802cb4147252da928c07ffd587cf
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2021-08-15 15:55:10 +0200

    extension-fixer

---
+++ utils/extension-gnome40-fix/extension-fixer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Fix Desktop Scroller
+sed -i 's/3\.30/40/' ~/.local/share/gnome-shell/extensions/desktop-scroller@brorlandi/metadata.json
+
+
+
+#Fix SimplerOffMenu
+sed -i 's/3\.36/40/' ~/.local/share/gnome-shell/extensions/SimplerOffMenu.kerkus@pm.me/metadata.json
+
+
