763ef683e69fed7c0bcef747248a4aaaabd51d07
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2021-12-22 11:33:05 +0100

    pgadmin line fix

--- dev-tools/software.sh
+++ dev-tools/software.sh
@@ -49,7 +49,6 @@ check_cmd() {
 # Add the repository key with either wget or curl
 if check_cmd apt-get; then # FOR DEB SYSTEMS
     ## pgAdmin
-    
     sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
     sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
     sudo apt install -y pgadmin4-desktop
