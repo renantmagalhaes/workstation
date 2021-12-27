b1caa68861dff74e7269bde5a2f6839f68d0c5b1
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2021-12-27 11:00:34 +0100

    new packages

--- dev-tools/devops.sh
+++ dev-tools/devops.sh
@@ -4,12 +4,35 @@ check_cmd() {
     command -v "$1" 2> /dev/null
 }
 
+# AWS 
+cd /tmp/
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+## EKS
+
+### aws-iam-authenticator
+brew install aws-iam-authenticator
+
+### eksctl
+brew tap weaveworks/tap
+brew install weaveworks/tap/eksctl
+
 
 # Ansible
 sudo pip3 install ansible
 sudo pip3 install "ansible-lint[yamllint]"
 sudo pip3 install argcomplete
 
+# Skaffold
+brew install skaffold
+
+# ArgoCD
+brew install argocd
+
+#FluxCD
+brew install fluxcd/tap/flux
 
 if check_cmd apt-get; then # FOR DEB SYSTEMS
     # Terraform
@@ -25,7 +48,10 @@ elif check_cmd dnf; then  # FOR RPM SYSTEMS
 
 elif check_cmd zypper; then  # FOR RPM SYSTEMS
     # Terraform
-    sudo zypper install -y terraform
+    # sudo zypper install -y terraform
+    brew tap hashicorp/tap
+    brew install hashicorp/tap/terraform
+
 
     echo "Not able to identify the system"
 fi
