9e94117790732a1338c8055dc305c466dfe413ed
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-01-01 12:18:11 +0100

    separate fonts installation from base scripts

--- desktop/0-DEB/0-gnome-system.sh
+++ desktop/0-DEB/0-gnome-system.sh
@@ -307,37 +307,10 @@ sudo apt-get -f install -y
 # Create git-folder
 mkdir -p ~/GIT-REPOS/CORE
 
-# Install Fonts
-git clone https://github.com/powerline/fonts.git ~/GIT-REPOS/CORE/fonts/
-bash ~/GIT-REPOS/CORE/fonts/install.sh
-
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -O ~/.local/share/fonts/PowerlineSymbols.otf
-
-mkdir -p ~/.config/fontconfig/conf.d/
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -O ~/.config/fontconfig/conf.d/10-powerline-symbols.conf
-
-git clone https://github.com/gabrielelana/awesome-terminal-fonts.git ~/GIT-REPOS/CORE/awesome-terminal-fonts
-sh -c "~/GIT-REPOS/CORE/awesome-terminal-fonts/install.sh"
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O ~/.local/share/fonts/FiraCode.zip
-unzip ~/.local/share/fonts/FiraCode.zip -d ~/.local/share/fonts/
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip -O ~/.local/share/fonts/3270.zip
-unzip ~/.local/share/fonts/3270.zip -d ~/.local/share/fonts/
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip -O ~/.local/share/fonts/Agave.zip
-unzip ~/.local/share/fonts/Agave.zip -d ~/.local/share/fonts/
-
-## cascadia font for vscode
-wget https://github.com/microsoft/cascadia-code/releases/download/v2105.24/CascadiaCode-2105.24.zip -O /tmp/CascadiaCode-2105.24.zip
-unzip /tmp/CascadiaCode-2105.24.zip -d /tmp/
-cp /tmp/ttf/CascadiaCodePL.ttf  ~/.local/share/fonts/
-cp /tmp/ttf/CascadiaCode.ttf  ~/.local/share/fonts/
-
-fc-cache -vf ~/.local/share/fonts/
+# Fonts
+bash desktop/source/any/fonts.sh
 
 # Themes
-
 bash desktop/source/gnome/themes.sh
 
 # Colorls
