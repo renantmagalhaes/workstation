1589176c7b5611b532f20637896d61f365b62a68
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-01-08 19:40:46 +0100

    global git config

--- utils/git-config/git-config.sh
+++ utils/git-config/git-config.sh
@@ -4,7 +4,7 @@
 
 git config --global user.email "renantmagalhaes@gmail.com"
 git config --global user.name "Renan Toesqui Magalhaes"
-
+git config --global pull.rebase false
 
 # show file
 cat ~/.gitconfig
