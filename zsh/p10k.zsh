a9559533ac124cb81dc4a121215337f3c455c8ed
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-02-27 20:49:52 +0100

    tmux experiments

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -92,9 +92,9 @@
     midnight_commander      # midnight commander shell (https://midnight-commander.org/)
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # vpn_ip                # virtual private network indicator
-    # load                  # CPU load
-    # disk_usage            # disk usage
-    # ram                   # free RAM
+    #load                  # CPU load
+    #disk_usage            # disk usage
+    #ram                   # free RAM
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
