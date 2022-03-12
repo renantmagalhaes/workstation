c679a97e5deffc3748104dfa2bfc706d70bc3626
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-03-12 17:09:56 +0100

    i3 initial tests

---
+++ desktop/utils/i3/fedora-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install i3-gaps on fedora
+
+sudo dnf install -y i3-gaps rofi polybar
+
+# Create folders
+mkdir -p ~/.config/polybar ~/.config/i3
+
+# I3 config
+ln -s -f $PWD/config/i3-config ~/.config/i3/config
+
+# Polybar config
+ln -s -f $PWD/config/polybar-config ~/.config/polybar/config
