e5a03a3933fa30ac2bbf53366160f097968458bf
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-03-12 18:10:45 +0100

    i3

--- desktop/utils/i3/fedora-install.sh
+++ desktop/utils/i3/fedora-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Install i3-gaps on fedora
-
+# Packages
 sudo dnf install -y i3-gaps rofi polybar
+sudo pip3 install pywal
 
 # Create folders
 mkdir -p ~/.config/polybar ~/.config/i3
@@ -13,3 +13,6 @@ ln -s -f $PWD/config/i3-config ~/.config/i3/config
 # Polybar config
 ln -s -f $PWD/config/polybar-config ~/.config/polybar/config
 ln -s -f $PWD/config/poly-launch.sh ~/.config/polybar/poly-launch.sh
+
+## Polybar themes
+#https://github.com/adi1090x/polybar-themes
