e119e208c2dd42b7fd6c19d592e8456a62bc68d4
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-03-19 12:28:22 +0100

    neovim update

--- desktop/source/any/vim.sh
+++ desktop/source/any/vim.sh
@@ -6,5 +6,6 @@ echo "set cryptmethod=blowfish2" >> ~/.vim/vimrc
 echo "set viminfo=" >> ~/.vim/vimrc
 
 # Neo VIM
+mv ~/.config/nvim ~/.config/nvim.BKP
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
