87d12e7ab0e402d834262aa6269ac11a73de15ff
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-03-20 18:59:39 +0100

    i3

--- desktop/utils/i3/fedora-install.sh
+++ desktop/utils/i3/fedora-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Packages
-sudo dnf install -y i3-gaps rofi polybar feh
+sudo dnf install -y i3-gaps rofi polybar feh lxappearance qt5-qtconfiguration picom
 sudo pip3 install pywal
 
 # Create folders
@@ -16,3 +16,10 @@ ln -s -f $PWD/config/poly-launch.sh ~/.config/polybar/poly-launch.sh
 
 ## Polybar themes
 #https://github.com/adi1090x/polybar-themes
+
+#XCAPE - Bind rofi to SuperKey
+sudo dnf install -y git gcc make pkgconfig libX11-devel libXtst-devel libXi-devel
+git clone https://github.com/alols/xcape.git ~/GIT-REPOS/CORE/xcape 
+cd ~/GIT-REPOS/CORE/xcape
+make
+sudo make install
