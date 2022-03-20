0a2b1f53d2f3216468633c8a87db245316f1d60b
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-03-20 22:04:33 +0100

    i3

--- desktop/utils/i3/fedora-install.sh
+++ desktop/utils/i3/fedora-install.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 # Packages
-sudo dnf install -y i3-gaps rofi polybar feh lxappearance qt5-qtconfiguration picom
+sudo dnf install -y i3-gaps rofi polybar feh lxappearance qt5-qtconfiguration picom blueman
 sudo pip3 install pywal
 
 # Create folders
-mkdir -p ~/.config/polybar ~/.config/i3 ~/.config/picom
+mkdir -p ~/.config/polybar ~/.config/i3 ~/.config/picom ~/.config/rofi
 
 # I3 config
-ln -s -f $PWD/config/i3-config ~/.config/i3/config
+ln -s -f $PWD/config/i3/i3-config ~/.config/i3/config
 
 # Polybar config
-ln -s -f $PWD/config/polybar-config ~/.config/polybar/config
-ln -s -f $PWD/config/poly-launch.sh ~/.config/polybar/poly-launch.sh
+ln -s -f $PWD/config/polybar/polybar-config ~/.config/polybar/config
+ln -s -f $PWD/config/polybar/poly-launch.sh ~/.config/polybar/poly-launch.sh
 
 # Picom config
-ln -s -f $PWD/config/picom.conf ~/.config/picom/picom.conf
+ln -s -f $PWD/config/picom/picom.conf ~/.config/picom/picom.conf
+
+# Rofi config
+ln -s -f $PWD/config/rofi/config.rasi ~/.config/rofi/config.rasi
 
 ## Polybar themes
 #https://github.com/adi1090x/polybar-themes
