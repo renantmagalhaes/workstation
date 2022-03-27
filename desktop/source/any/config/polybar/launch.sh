53abbf3b8b128bb6d06d51bb4c7e2b394b689e83
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-03-27 19:01:33 +0200

    polybar kde panel

--- desktop/source/any/config/polybar/launch.sh
+++ desktop/source/any/config/polybar/launch.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env sh
 
+# if in kde, this will display tray icons
+pidof xembedsniproxy |xargs kill -9
+
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# Launch bars
 polybar main &
-polybar secondary &
+# polybar secondary &
 
-#polybar mybar &
 
 echo "Bars launched..."
