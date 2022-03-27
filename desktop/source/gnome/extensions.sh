22fc0ae9e849a4014cd1ea468226e41a258a7eea
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-03-27 10:40:28 +0200

    gnome extensions

--- desktop/source/gnome/extensions.sh
+++ desktop/source/gnome/extensions.sh
@@ -3,3 +3,18 @@
 git clone https://github.com/andyrichardson/simply-workspaces.git ~/.local/share/gnome-shell/extensions/simply.workspaces@andyrichardson.dev
 dconf write /org/gnome/shell/overrides/dynamic-workspaces false
 dconf write /org/gnome/desktop/wm/preferences/num-workspaces 4
+
+# Mutter Rounded (plus blur effect)
+# https://github.com/yilozt/mutter-rounded
+
+# Media Controls
+#https://extensions.gnome.org/extension/4470/media-controls/
+
+# BaBar Task Bar
+#https://extensions.gnome.org/extension/4000/babar/
+
+# Big Sur Status Area
+#https://extensions.gnome.org/extension/4085/big-sur-status-area/
+
+# Resource Monitor
+#https://extensions.gnome.org/extension/1634/resource-monitor/
