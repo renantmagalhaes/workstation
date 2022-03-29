a81e145da07066e41838ae5bf2c78af9c32df748
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-03-29 18:50:08 +0200

    bluetooth manager for i3

--- desktop/source/i3/install.sh
+++ desktop/source/i3/install.sh
@@ -3,11 +3,11 @@
 # Packages
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf install -y i3-gaps feh lxappearance qt5-qtconfiguration picom playerctl
+sudo dnf install -y i3-gaps feh lxappearance qt5-qtconfiguration picom playerctl blueman
 sudo pip3 install pywal
 
 # Create folders
-mkdir -p ~/.config/i3 ~/.config/picom/.local/share/fonts
+mkdir -p ~/.config/i3 ~/.config/picom/ ~/.local/share/fonts
 
 # I3 config
 ln -s -f $PWD/config/i3/i3-config ~/.config/i3/config
