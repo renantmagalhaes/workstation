b79c1e1a55abd7ece5dbf4c358e77c1f3851bcbf
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-04-03 17:58:08 +0200

    i3 templates

--- desktop/source/any/config/polybar/launch.sh
+++ desktop/source/any/config/polybar/launch.sh
@@ -1,4 +1,27 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# dir="$HOME/.config/polybar"
+# themes=(`ls --hide="launch.sh" $dir`)
+
+# launch_bar() {
+# 	# Terminate already running bar instances
+# 	killall -q polybar
+
+# 	# Wait until the processes have been shut down
+# 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# 	# Launch the bar
+# 	polybar -q main -c "$dir/$style/config.ini" &
+#     sleep 1
+#     polybar -q secondary -c "$dir/$style/config.ini" &
+# }
+
+
+# style="panels"
+# launch_bar
+
+
+#
 
 # if in kde, this will display tray icons
 pidof xembedsniproxy |xargs kill -9
@@ -17,3 +40,6 @@ polybar secondary &
 
 
 echo "Bars launched..."
+
+
+
