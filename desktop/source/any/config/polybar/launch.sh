3855742a3fb042561e9f6ff5e7297dccc2f0114f
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-04-03 21:35:57 +0200

    fix poweroff menu

--- desktop/source/any/config/polybar/launch.sh
+++ desktop/source/any/config/polybar/launch.sh
@@ -1,32 +1,5 @@
 #!/usr/bin/env bash
 
-# dir="$HOME/.config/polybar"
-# themes=(`ls --hide="launch.sh" $dir`)
-
-# launch_bar() {
-# 	# Terminate already running bar instances
-# 	killall -q polybar
-
-# 	# Wait until the processes have been shut down
-# 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# 	# Launch the bar
-# 	polybar -q main -c "$dir/$style/config.ini" &
-#     sleep 1
-#     polybar -q secondary -c "$dir/$style/config.ini" &
-# }
-
-
-# style="panels"
-# launch_bar
-
-
-#
-
-# if in kde, this will display tray icons
-pidof xembedsniproxy |xargs kill -9
-
-
 # Terminate already running bar instances
 killall -q polybar
 
@@ -41,5 +14,6 @@ polybar secondary &
 
 echo "Bars launched..."
 
-
+# if in kde, this will display tray icons
+pidof xembedsniproxy |xargs kill -9
 
