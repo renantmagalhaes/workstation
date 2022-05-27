c7745b6077ec73b90fdfa9f20508fd1d6d94628f
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-05-28 00:29:14 +0200

    increase sleep time

--- desktop/source/bspwm/config/scripts/rtm-startup-session.sh
+++ desktop/source/bspwm/config/scripts/rtm-startup-session.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-sleep 1
-
 bspc desktop --focus 1 & vivaldi &
-sleep 0.5
+sleep 1
 bspc desktop --focus 11 ; vivaldi  &
 vivaldi https://youtube.com &
-sleep 0.5
+sleep 1
 bspc desktop --focus 2 ; /opt/vivaldi/vivaldi --profile-directory=Default --app-id=hnpfjngllnobngcgfapefoaidbinmjnm &
 sleep 0.5
 bspc desktop --focus 22 ; /opt/vivaldi/vivaldi --profile-directory=Default --app-id=majiogicmcnmdhhlgmkahaleckhjbmlk &
