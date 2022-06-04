26b6d893b8afe866472b0bcfbbba19ac63c4920a
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-06-04 17:38:37 +0200

    change alt style

--- desktop/source/any/config/rofi/scripts/powermenu/powermenu.sh
+++ desktop/source/any/config/rofi/scripts/powermenu/powermenu.sh
@@ -15,7 +15,7 @@
 # full_circle     full_square     full_rounded     full_alt
 # row_circle      row_square      row_rounded      row_alt
 
-theme="card_square"
+theme="card_alt"
 dir="$HOME/.config/rofi/scripts/powermenu"
 
 # random colors
