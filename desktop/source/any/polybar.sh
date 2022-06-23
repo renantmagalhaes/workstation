ba0b2f5d6c8653d424e145e516c63304e4931d7f
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-06-23 12:50:27 +0200

    fix syntax

--- desktop/source/any/polybar.sh
+++ desktop/source/any/polybar.sh
@@ -7,7 +7,16 @@ check_cmd() {
 # Add the repository key with either wget or curl
 if check_cmd apt-get; then # FOR DEB SYSTEMS
     #pgk
-    sudo apt-get install -y polybar mpd wmctrl playerctl fonts-material-design-icons-iconfont fonts-materialdesignicons-webfont yad xsel
+    sudo apt-get install -y mpd wmctrl playerctl fonts-material-design-icons-iconfont fonts-materialdesignicons-webfont yad xsel
+
+    # compile polybar
+    # Dependencies
+    sudo apt-get install -y cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python3-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev libxcb-composite0-dev libjsoncpp-dev python3-sphinx libuv1-dev
+    sudo ln -s /usr/include/jsoncpp/json/ /usr/include/json
+
+    git clone https://github.com/jaagr/polybar.git ~/GIT-REPOS/CORE/polybar
+    cd ~/GIT-REPOS/CORE/polybar && ./build.sh
+
 
 elif check_cmd dnf; then  # FOR RPM SYSTEMS
     # pkg
