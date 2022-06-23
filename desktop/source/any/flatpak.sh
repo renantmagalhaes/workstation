49e255c088c9146c7e3e16049cc87311e8b433d2
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-06-23 09:54:10 +0200

    change nvim installation to brew

--- desktop/source/any/flatpak.sh
+++ desktop/source/any/flatpak.sh
@@ -31,6 +31,3 @@ sudo flatpak install -y flathub com.obsproject.Studio
 ## Telegram
 # sudo flatpak install -y flathub org.telegram.desktop
 # sudo flatpak override --filesystem=home:ro org.telegram.desktop
-
-# neovim
-sudo flatpak install -y flathub io.neovim.nvim
