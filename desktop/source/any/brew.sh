31a6af1a19d7a52ef712eafd984442150b50cf89
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-06-23 11:17:23 +0200

    update pop 22.04 installation

--- desktop/source/any/brew.sh
+++ desktop/source/any/brew.sh
@@ -3,6 +3,8 @@
 # install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/rtm/.zprofile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/rtm/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install neovim
 brew install neovim
