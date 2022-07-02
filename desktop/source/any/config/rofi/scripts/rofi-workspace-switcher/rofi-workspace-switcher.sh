36368bd004cd448b2879d58e7f99a1a2640c1f46
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-07-03 00:25:38 +0200

    PoC in progress

---
+++ desktop/source/any/config/rofi/scripts/rofi-workspace-switcher/rofi-workspace-switcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "    "
+echo "    "
+
+# find number of workspaces on primary monitor
+
+# multiply the icon * number the worspaces (1 monitor)
+
+# change the icon from the current workspace
