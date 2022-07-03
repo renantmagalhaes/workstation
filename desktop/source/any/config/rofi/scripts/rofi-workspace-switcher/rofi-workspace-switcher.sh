2f40ab3a187e77743cf0017778dd469678f6cf81
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-07-03 15:17:49 +0200

    add tasks

--- desktop/source/any/config/rofi/scripts/rofi-workspace-switcher/rofi-workspace-switcher.sh
+++ desktop/source/any/config/rofi/scripts/rofi-workspace-switcher/rofi-workspace-switcher.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+#TODO
+#// find number of workspaces on primary monitor
+# multiply the icon * number the worspaces (1 monitor)
+# loop in a list with a counter and print the icons of the worspaces. if workspace == get_current_workspace print full icon, else print empty icon
+# change the icon from the current workspace
+
+
+
+GET_TOTAL_NUM_WORKSPACES=`bspc query -D  --names |wc -l`
+GET_TOTAL_NUM_MONITORS=`xrandr --listactivemonitors |wc -l`
+
+
+GET_CURRENT_WORKSPACE=`bspc query -D -d focused --names`
+NUM_WORKSPACES=$((GET_TOTAL_NUM_WORKSPACES / (GET_TOTAL_NUM_MONITORS - 1)))
+
+
+
 echo "    "
 echo "    "
-
-# find number of workspaces on primary monitor
-
-# multiply the icon * number the worspaces (1 monitor)
-
-# change the icon from the current workspace
