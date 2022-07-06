ded77068df5f46b590a9bcf8fec6baf8726d84d5
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-07-06 10:35:13 +0200

    update with latest configs

--- desktop/windows/wsl-ubuntu.sh
+++ desktop/windows/wsl-ubuntu.sh
@@ -28,12 +28,12 @@ sudo apt-get -y install zsh fonts-powerline vim tmux curl net-tools iproute2 git
 sudo apt-get -f install -y
 
 # Brew
-bash desktop/source/any/brew.sh
+bash ../source/any/brew.sh
 
 #Utils
 
 ## LSD
-wget https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_amd64.deb -O /tmp/lsd_amd64.deb
+wget https://github.com/Peltoche/lsd/releases/download/0.22.0/lsd-musl_0.22.0_amd64.deb -O /tmp/lsd_amd64.deb
 sudo dpkg -i /tmp/lsd_amd64.deb
 
 ## Fix python default path
@@ -46,42 +46,14 @@ sudo pip3 install bpytop --upgrade
 sudo apt-get -f install -y
 
 # New VIM
-sudo apt-get install -y build-essential
-curl -sLf https://spacevim.org/install.sh | bash
-echo "set ignorecase" >> ~/.vim/vimrc
-echo "set cryptmethod=blowfish2" >> ~/.vim/vimrc
-echo "set viminfo=" >> ~/.vim/vimrc
-sudo apt-get -f install -y
+bash ../source/any/vim.sh
 
 # Create git-folder 
 mkdir -p ~/GIT-REPOS/CORE
 
 # Install Fonts
-git clone https://github.com/powerline/fonts.git ~/GIT-REPOS/CORE/fonts/
-bash ~/GIT-REPOS/CORE/fonts/install.sh
-
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -O ~/.local/share/fonts/PowerlineSymbols.otf
-
-mkdir -p ~/.config/fontconfig/conf.d/
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -O ~/.config/fontconfig/conf.d/10-powerline-symbols.conf
-
-git clone https://github.com/gabrielelana/awesome-terminal-fonts.git ~/GIT-REPOS/CORE/awesome-terminal-fonts
-sh -c "~/GIT-REPOS/CORE/awesome-terminal-fonts/install.sh"
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -O ~/.local/share/fonts/FiraCode.zip
-unzip ~/.local/share/fonts/FiraCode.zip -d ~/.local/share/fonts/
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip -O ~/.local/share/fonts/3270.zip
-unzip ~/.local/share/fonts/3270.zip -d ~/.local/share/fonts/
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip -O ~/.local/share/fonts/Agave.zip
-unzip ~/.local/share/fonts/Agave.zip -d ~/.local/share/fonts/
-
-## cascadia font for vscode
-wget https://github.com/microsoft/cascadia-code/releases/download/v2105.24/CascadiaCode-2105.24.zip -O /tmp/CascadiaCode-2105.24.zip
-unzip /tmp/CascadiaCode-2105.24.zip -d /tmp/
-cp /tmp/ttf/CascadiaCodePL.ttf  ~/.local/share/fonts/
-cp /tmp/ttf/CascadiaCode.ttf  ~/.local/share/fonts/
+# Fonts
+bash ../source/any/fonts.sh
 
 # Colorls
 sudo apt install -y ruby-dev
