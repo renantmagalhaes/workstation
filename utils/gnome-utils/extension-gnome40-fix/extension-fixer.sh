b70e6796389b6e9196b3fd8e52388a324e6d74a3
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-07-10 00:48:43 +0200

    nome

--- utils/gnome-utils/extension-gnome40-fix/extension-fixer.sh
+++ utils/gnome-utils/extension-gnome40-fix/extension-fixer.sh
@@ -5,11 +5,14 @@ rm -rf ~/.local/share/gnome-shell/extensions/desktop-scroller@brorlandi
 git clone https://github.com/BrOrlandi/Desktop-Scroller-GNOME-Extension.git ~/.local/share/gnome-shell/extensions/desktop-scroller@brorlandi
 
 #Fix Desktop Scroller
-sed -i 's/3\.30/41/' ~/.local/share/gnome-shell/extensions/desktop-scroller@brorlandi/metadata.json
+sed -i 's/3\.30/42/' ~/.local/share/gnome-shell/extensions/desktop-scroller@brorlandi/metadata.json
 
-
-
-#Fix SimplerOffMenu
+# Fix SimplerOffMenu
 sed -i 's/3\.36/40/' ~/.local/share/gnome-shell/extensions/SimplerOffMenu.kerkus@pm.me/metadata.json
+
+# Multi Monitor
+git clone https://github.com/realh/multi-monitors-add-on.git ~/GIT-REPOS/CORE/multi-monitors-add-on
+cd ~/GIT-REPOS/CORE/multi-monitors-add-on
+cp -r multi-monitors-add-on@spin83 ~/.local/share/gnome-shell/extensions/
 
 
