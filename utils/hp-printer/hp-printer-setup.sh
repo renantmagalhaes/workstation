e4c8ba5b28adc149a3a76aa8990e2bbc567e4d8f
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-07-20 22:39:29 +0200

    setup hp printer

---
+++ utils/hp-printer/hp-printer-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# check cmd function
+check_cmd() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+if check_cmd apt-get; then # FOR DEB SYSTEMS
+    sudo apt-get install -y install hplip xsane sane
+    hp-setup
+elif check_cmd dnf; then
+    dnf install -y hplip xsane sane
+    hp-setup
+else
+    echo "system not found"
+    fi
