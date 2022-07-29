5a20948f2a6b2566b57d1edac9ba62082db145f1
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-07-29 22:12:09 +0200

    change commands

--- desktop/source/any/config/rofi/scripts/rofi-powermenu/rtm-powermenu.sh
+++ desktop/source/any/config/rofi/scripts/rofi-powermenu/rtm-powermenu.sh
@@ -3,8 +3,8 @@
 chosen=$(printf "  Power Off\n  Restart\n  Lock" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
-        "  Power Off") poweroff ;;
-        "  Restart") reboot ;;
+        "  Power Off") shutdown -h now ;;
+        "  Restart") shutdown -r now ;;
         "  Lock") ~/.config/bspwm/scripts/blur-lock ;;
         *) exit 1 ;;
 esac
