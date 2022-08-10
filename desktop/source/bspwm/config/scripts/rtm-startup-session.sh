39f4a0f4008522224c2309d7d31a044b14e6d4d1
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-08-10 18:13:36 +0200

    adjust times

--- desktop/source/bspwm/config/scripts/rtm-startup-session.sh
+++ desktop/source/bspwm/config/scripts/rtm-startup-session.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-sleep 1
 bspc desktop --focus 1 ; vivaldi &
-sleep 1
+sleep 2
 bspc desktop --focus 11 ; vivaldi  &
 vivaldi https://youtube.com &
-sleep 1
+sleep 2
 bspc desktop --focus 2 ; /opt/vivaldi/vivaldi --profile-directory=Default --app-id=hnpfjngllnobngcgfapefoaidbinmjnm &
-sleep 0.5
+sleep 1
 bspc desktop --focus 22 ; /opt/vivaldi/vivaldi --profile-directory=Default --app-id=majiogicmcnmdhhlgmkahaleckhjbmlk &
 sleep 0.5
 bspc desktop --focus 5
