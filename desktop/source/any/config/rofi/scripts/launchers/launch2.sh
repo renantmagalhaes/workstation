5987a30a6dc2960da0a39669aa6045d13b6581ab
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-08-19 22:20:56 +0200

    add rofi launchers

--- desktop/source/any/config/rofi/scripts/launchers/launch2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-theme="default2"
-dir="$HOME/.config/rofi/scripts/launchers"
-
-rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
---
+++ desktop/source/any/config/rofi/scripts/launchers/type-1/tempCodeRunnerFile.sh
@@ -0,0 +1,1 @@
+/home/rtm/GIT-REPOS/CORE/rofi/files/launchers
