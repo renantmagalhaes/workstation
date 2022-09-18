26f77329679d211c9adf13adbdf8a58475aae851
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-09-18 14:42:13 +0200

    add grub2 customizer

---
+++ utils/grub2-customizer/custom-grub2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Clone repo
+git clone https://github.com/vinceliuice/grub2-themes ~/GIT-REPOS/CORE/grub2-themes
+
+
+# Install with options
+sh -c "~/GIT-REPOS/CORE/grub2-themes/install.sh -t stylish -s 2k"
