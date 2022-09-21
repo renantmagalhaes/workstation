a0fe596fd1a023ac62f53242cc591239310f2bb9
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-09-21 22:37:48 +0200

    add gdm customizer

---
+++ utils/gnome-utils/gdm-customizer/gdm-customizer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://github.com/realmazharhussain/gdm-settings
+# https://github.com/realmazharhussain/gdm-settings/wiki/Install
+
+# sudo dnf install -y xdg-desktop-portal-gnome
+
+sudo flatpak install -y io.github.realmazharhusain.GdmSettings
