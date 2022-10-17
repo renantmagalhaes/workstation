42f649c05d8e6fc5522c4a693b5f9b96d6022b52
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-10-17 19:26:48 +0200

    add hydrapaper for multi wallpaper support

--- desktop/source/gnome/flatpak.sh
+++ desktop/source/gnome/flatpak.sh
@@ -3,3 +3,5 @@
 # Flatpack repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# hydrapaper for dual wallpapers
+sudo flatpak install -y flathub org.gabmus.hydrapaper
