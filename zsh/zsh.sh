efd50e5e12c7deee013d1208b75ca3c6b28c3219
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2022-12-04 14:37:53 +0100

    fix sed pattern

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -4,6 +4,9 @@ type zsh >/dev/null 2>&1 || { echo >&2 "Install zsh before run this script "; ex
 #verify curl
 type curl >/dev/null 2>&1 || { echo >&2 "Install zsh before run this script "; exit 1; }
 
+macos_check=`uname -a |awk '{print $1}' | awk '{print tolower($0)}'`
+linux_check=`uname -a |awk '{print $1}' | awk '{print tolower($0)}'`
+
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -20,15 +23,26 @@ ln -s -f $PWD/lsd-config.yaml ~/.config/lsd/config.yaml
 #install plugins
 ##zsh-syntax-highlighting.zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 ##zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 ##zsh-completions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 autoload -U compinit && compinit
+
 ## enhancd
 git clone https://github.com/b4b4r07/enhancd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/enhancd
-sed -i 's/\.\./\./g' ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/enhancd/init.sh
-sed -i 's/ENHANCD_DISABLE_HYPHEN\:\-0/ENHANCD_DISABLE_HYPHEN\:\-1/g' ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/enhancd/init.sh
+
+if [[ $macos_check == "darwin" ]]; then
+    sed -i '' 's/\.\./\./g' ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/enhancd/init.sh
+    sed -i '' 's/ENHANCD_DISABLE_HYPHEN\:\-0/ENHANCD_DISABLE_HYPHEN\:\-1/g' ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/enhancd/init.sh
+elif [[ $linux_check == "linux" ]]; then
+    sed -i 's/\.\./\./g' ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/enhancd/init.sh
+    sed -i 's/ENHANCD_DISABLE_HYPHEN\:\-0/ENHANCD_DISABLE_HYPHEN\:\-1/g' ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/enhancd/init.sh
+else
+echo "Not able to identify desktop environment"
+fi
 
 #install powerlevel9k
 #git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
