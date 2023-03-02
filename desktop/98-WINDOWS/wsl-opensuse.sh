f463f425f327849ed733075b5efe990885ffc8e3
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-03-02 11:17:34 +0100

    update to tumbleweed usage

--- desktop/98-WINDOWS/wsl-opensuse.sh
+++ desktop/98-WINDOWS/wsl-opensuse.sh
@@ -22,17 +22,17 @@ sudo ntpdate pool.ntp.org
 
 
 # Install the packages from repo
-sudo zypper install -y zsh vim tmux curl net-tools iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak xournal evince jq
+sudo zypper install -y zsh vim tmux curl net-tools iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak neofetch evince jq firefox
 
 # Install SNAP
-sudo zypper addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Leap_`cat /etc/os-release |grep VERSION_ID |egrep -oh '[0-9][0-9]\.[0-9]'` snappy
+sudo zypper addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Tumbleweed snappy
 sudo zypper --gpg-auto-import-keys refresh
 sudo zypper dup --from snappy
-sudo zypper install snapd
+sudo zypper install -y snapd
 
 
 # Brew
-bash desktop/source/any/brew.sh
+bash ../source/any/brew.sh
 
 #Utils
 
