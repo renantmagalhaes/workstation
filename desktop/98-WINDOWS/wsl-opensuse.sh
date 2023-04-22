27dc950cba404c2d2b2e2922c941bd9a44348d66
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-04-22 11:50:17 +0100

    fix WSL bugs for TW

--- desktop/98-WINDOWS/wsl-opensuse.sh
+++ desktop/98-WINDOWS/wsl-opensuse.sh
@@ -22,10 +22,11 @@ sudo ntpdate pool.ntp.org
 
 
 # Install the packages from repo
-sudo zypper install -y zsh vim curl net-tools iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak neofetch evince jq firefox net-tools-deprecated busybox-sysvinit-tools
+sudo zypper install -y zsh vim curl net-tools iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak neofetch evince jq firefox net-tools-deprecated busybox-sysvinit-tools bind-utils
 
 # Install SNAP
-sudo zypper addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Leap_`cat /etc/os-release |grep VERSION_ID |egrep -Eoh '[0-9]{1,2}.[0-9]'` snappy
+#sudo zypper addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Leap_`cat /etc/os-release |grep VERSION_ID |egrep -Eoh '[0-9]{1,2}.[0-9]'` snappy
+sudo zypper addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Tumbleweed snappy
 sudo zypper --gpg-auto-import-keys refresh
 sudo zypper dup --from snappy
 sudo zypper install -y snapd
@@ -35,7 +36,7 @@ sudo zypper install -y snapd
 bash ../source/any/brew.sh
 
 # Temporary install Tmux via brew
-brew install tmux
+/home/linuxbrew/.linuxbrew/bin/brew install tmux
 
 #Utils
 
