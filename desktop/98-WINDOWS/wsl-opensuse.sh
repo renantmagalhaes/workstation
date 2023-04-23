321f5577d6ea2ed4ecef0983339c7f08509d28ad
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-04-23 10:42:11 +0100

    add depedency to make nvimchad compile 100% of plugins

--- desktop/98-WINDOWS/wsl-opensuse.sh
+++ desktop/98-WINDOWS/wsl-opensuse.sh
@@ -22,7 +22,7 @@ sudo ntpdate pool.ntp.org
 
 
 # Install the packages from repo
-sudo zypper install -y zsh vim curl net-tools iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak neofetch evince jq firefox net-tools-deprecated busybox-sysvinit-tools bind-utils
+sudo zypper install -y zsh vim curl net-tools iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak neofetch evince jq firefox net-tools-deprecated busybox-sysvinit-tools bind-utils gcc-c++
 
 # Install SNAP
 #sudo zypper addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Leap_`cat /etc/os-release |grep VERSION_ID |egrep -Eoh '[0-9]{1,2}.[0-9]'` snappy
