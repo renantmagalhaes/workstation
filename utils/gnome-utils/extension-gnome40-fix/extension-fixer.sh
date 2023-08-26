f787e11d5437d7702ccac338743707497cc264f7
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-08-26 21:11:46 +0100

    fix height

--- utils/gnome-utils/extension-gnome40-fix/extension-fixer.sh
+++ utils/gnome-utils/extension-gnome40-fix/extension-fixer.sh
@@ -5,7 +5,7 @@ rm -rf ~/.local/share/gnome-shell/extensions/desktop-scroller@brorlandi
 git clone https://github.com/BrOrlandi/Desktop-Scroller-GNOME-Extension.git ~/.local/share/gnome-shell/extensions/desktop-scroller@brorlandi
 
 #Fix Desktop Scroller
-sed -i 's/40/42/' ~/.local/share/gnome-shell/extensions/desktop-scroller@brorlandi/metadata.json
+sed -i 's/40/43/' ~/.local/share/gnome-shell/extensions/desktop-scroller@brorlandi/metadata.json
 
 # Fix SimplerOffMenu
 sed -i 's/3\.36/40/' ~/.local/share/gnome-shell/extensions/SimplerOffMenu.kerkus@pm.me/metadata.json
