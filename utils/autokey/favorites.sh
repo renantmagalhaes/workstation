44afb2edc3316027efac81fc330f6980b3555899
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-09-02 19:32:56 +0100

    optimization logic

--- utils/autokey/favorites.sh
+++ utils/autokey/favorites.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 
-# Function to get the active window ID
-get_active_window_id() {
-    xdotool getactivewindow
-}
-
-# Function to get the active window name
-get_active_window_name() {
-    xdotool getwindowname $(get_active_window_id)
-}
 
 # Get the active window's name
-window_name=$(get_active_window_name)
+window_name=$(xdotool getactivewindow getwindowname)
 echo "$window_name"
 
 # Check if the window name contains "Microsoft" followed by any character and then "Edge"
 if [[ $window_name =~ Microsoft.+Edge ]]; then
-    # Activate (focus and raise) the Microsoft Edge window
-    xdotool windowactivate $(get_active_window_id)
-    # Introduce a delay to ensure the window is activated
-    sleep 1
     # Simulate Ctrl+Shift+O using xdotool
+    sleep 0.1
     xdotool key ctrl+shift+o
+
 else
     echo "Active window is not Microsoft Edge."
 fi
