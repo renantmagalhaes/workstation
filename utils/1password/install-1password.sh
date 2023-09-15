7e006b8e8bb45abd766549be29da600321cab383
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-09-15 16:27:01 +0200

    fix 1password installation from main menu

--- utils/1password/install-1password.sh
+++ utils/1password/install-1password.sh
@@ -12,27 +12,22 @@ if check_cmd apt-get; then # FOR DEB SYSTEMS
     sudo dpkg -i /tmp/1password-latest.deb
     sudo apt-get -f install
 
-elif check_cmd dnf; then  # FOR RPM SYSTEMS
-    wget https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm -O /tmp/1password-latest.rpm
-    sudo rpm -i /tmp/1password-latest.rpm
-
 elif check_cmd zypper; then  # FOR SUSE SYSTEMS
     wget https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm -O /tmp/1password-latest.rpm
     sudo rpm -i /tmp/1password-latest.rpm
-
-
 
 else
     echo "Not able to identify the system"
     exit 0
 fi
 
-# Fix vivaldi
-sudo mkdir -p  /etc/1password
-sudo cp custom_allowed_browsers /etc/1password/custom_allowed_browsers
-sudo chown root:root /etc/1password/custom_allowed_browsers && sudo chmod 755 /etc/1password/custom_allowed_browsers
+# # Fix vivaldi
+# sudo mkdir -p  /etc/1password
+# sudo cp custom_allowed_browsers /etc/1password/custom_allowed_browsers
+# sudo chown root:root /etc/1password/custom_allowed_browsers && sudo chmod 755 /etc/1password/custom_allowed_browsers
 
 # Auto start
+sudo mkdir -p ~/.config/autostart/
 bash -c 'cat << EOF > ~/.config/autostart/1password.desktop
 [Desktop Entry]
 Type=Application
