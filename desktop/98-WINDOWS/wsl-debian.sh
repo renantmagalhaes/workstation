5a65355fbabfd4a439426c4f35a0a92c0f08ed86
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-09-15 11:57:58 +0200

    add packages for themes

--- desktop/98-WINDOWS/wsl-debian.sh
+++ desktop/98-WINDOWS/wsl-debian.sh
@@ -24,7 +24,7 @@ sudo ntpdate pool.ntp.org
 sudo apt-get update && sudo apt-get -y upgrade
 
 # Install the packages from repo
-sudo apt-get -y install zsh fonts-powerline vim wget tmux curl net-tools iproute2 git fonts-hack-ttf apt-transport-https htop meld tree nload pwgen sysstat xclip unrar-free unzip python3 python3-pip net-tools ncdu whois flatpak snapd xournal evince jq bpytop virtualenv virtualenvwrapper pylint dnsutils nala
+sudo apt-get -y install zsh fonts-powerline vim wget tmux curl net-tools iproute2 git fonts-hack-ttf apt-transport-https htop meld tree nload pwgen sysstat xclip unrar-free unzip python3 python3-pip net-tools ncdu whois flatpak snapd xournal evince jq bpytop virtualenv virtualenvwrapper pylint dnsutils nala sassc
 
 # Flatpack
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
