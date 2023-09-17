66336cc0fd43223a286312fe20614a32e68cdad1
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-09-17 17:03:09 +0100

    change edge for brave

--- utils/autokey/favorites.sh
+++ utils/autokey/favorites.sh
@@ -2,13 +2,13 @@
 
 
 # Get the active window's name
-window_name=$(xdotool getactivewindow getwindowname |grep Edge |awk '{print $NF}')
+window_name=$(xdotool getactivewindow getwindowname |grep Brave |awk '{print $NF}')
 echo "$window_name"
 
 # Check if the window name contains "Microsoft" followed by any character and then "Edge"
-if [[ $window_name == "Edge" ]]; then
+if [[ $window_name == "Brave" ]]; then
     # Simulate Ctrl+Shift+O using xdotool
-    sleep 0.1
+    sleep 0.2
     xdotool key ctrl+shift+o
 else
     sleep 0.1
