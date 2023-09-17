9794c20787d9100eee4c5a65d60053aedbf2b436
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-09-17 23:34:16 +0100

    add loop to be sure search window is set to float

--- utils/autokey/search.sh
+++ utils/autokey/search.sh
@@ -3,14 +3,20 @@
 
 # Get the active window's name
 window_name=$(xdotool getactivewindow getwindowname |grep Edge |awk '{print $NF}')
-echo "$window_name"
+edge_search_window_name=$(xdotool getactivewindow getwindowname)
+
 
 # Check if the window name contains "Microsoft" followed by any character and then "Edge"
 if [[ $window_name == "Edge" ]]; then
     # Simulate Ctrl+Shift+O using xdotool
     sleep 0.1
     xdotool key ctrl+shift+a
-    sleep 0.4
+    sleep 0.1
+    while [[ $edge_search_window_name != "" ]]
+    do
+        sleep 0.05
+        edge_search_window_name=$(xdotool getactivewindow getwindowname)
+    done
     xdotool key ctrl+shift+F12
 else
     sleep 0.1
