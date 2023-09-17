acaf2817f4d6b19e66ebcc71550021273290b551
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-09-17 22:19:53 +0100

    edge improvements

--- utils/autokey/search.sh
+++ utils/autokey/search.sh
@@ -2,14 +2,16 @@
 
 
 # Get the active window's name
-window_name=$(xdotool getactivewindow getwindowname |grep Brave |awk '{print $NF}')
+window_name=$(xdotool getactivewindow getwindowname |grep Edge |awk '{print $NF}')
 echo "$window_name"
 
 # Check if the window name contains "Microsoft" followed by any character and then "Edge"
-if [[ $window_name == "Brave" ]]; then
+if [[ $window_name == "Edge" ]]; then
     # Simulate Ctrl+Shift+O using xdotool
-    sleep 0.2
+    sleep 0.1
     xdotool key ctrl+shift+a
+    sleep 0.4
+    xdotool key ctrl+shift+F12
 else
     sleep 0.1
     pkill sxhkd
