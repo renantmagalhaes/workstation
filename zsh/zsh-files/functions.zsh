ed2a526ae56ffb0403d319e5775a703a0bbe1763
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-10-26 15:45:20 +0200

    zsh new folder structure

---
+++ zsh/zsh-files/functions.zsh
@@ -0,0 +1,50 @@
+# check cmd function
+check_cmd() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+#RTM
+macos_check=`uname -a |awk '{print $1}' | awk '{print tolower($0)}'`
+linux_check=`uname -a |awk '{print $1}' | awk '{print tolower($0)}'`
+
+# Colormap
+function colormap() {
+  for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
+}
+
+### pw generator
+function pw () {
+    pwgen -sync "${1:-48}" -1 | if command -v pbcopy > /dev/null 2>&1; then pbcopy; else xclip; fi
+}
+
+### get weather
+function weather () {
+    curl "https://wttr.in/${1}"
+}
+
+function ip-geolocation() {
+    curl http://ip-api.com/json/$1 | jq .
+}
+
+### find alias
+function find-folder () {
+find $1 -type d -iname $2 -exec bat {} +
+}
+
+function find-file () {
+find $1 -type f -iname $2 -exec bat {} +
+}
+
+## docker remove all
+
+function docker-remove-all () {
+    sudo docker stop $(sudo docker ps -aq)
+    sudo docker rm $(sudo docker ps -aq)
+    sudo docker rmi $(sudo docker images -q)
+    sudo docker volume rm $(sudo docker volume ls -q)
+    sudo docker network rm $(sudo docker network ls -q)
+}
+
+function copydir {
+  pwd | tr -d "\r\n" | xclip
+}
