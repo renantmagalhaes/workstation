37cec711820e63395cf66efe13df05305c94c2f6
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2023-12-24 13:21:03 +0000

    update droidcam latest version 2.1.2

--- desktop/0-DEB/0-gnome-system.sh
+++ desktop/0-DEB/0-gnome-system.sh
@@ -144,7 +144,7 @@ sudo gem install colorls
 
 ## Droidcam
 cd /tmp/
-wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_2.0.0.zip
+wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_2.1.2.zip
 unzip droidcam_latest.zip -d droidcam
 cd droidcam && sudo ./install-client
 sudo ./install-video
