bbd8ba19712cff777886023eaf3f0d17596b5485
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-04 13:20:18 +0100

    add auto-pair plugin

---
+++ desktop/source/any/config/nvim/lua/plugins/auto-pair.lua
@@ -0,0 +1,9 @@
+return{
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  }
+}
