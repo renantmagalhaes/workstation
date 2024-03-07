8664341a1c7f85fecd8bcc72e85d5678b77a5788
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-07 21:21:18 +0000

    scroll-opstions

--- desktop/source/any/config/nvim/lua/plugins/scroll-misc.lua
+++ desktop/source/any/config/nvim/lua/plugins/scroll-misc.lua
@@ -20,5 +20,11 @@ return {
         -- hlslens config overrides
       })
     end,
+    {
+      "karb94/neoscroll.nvim",
+      config = function()
+        require("neoscroll").setup({})
+      end,
+    },
   },
 }
