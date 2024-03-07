658ad7fb8aa985f1e6fda2ebbc62bd138a8e3454
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-07 18:37:57 +0000

    Search and scrollbar plugins

---
+++ desktop/source/any/config/nvim/lua/plugins/scroll-bar.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "petertriho/nvim-scrollbar",
+    config = function()
+      require("scrollbar").setup({
+        -- excluded_filetypes = { "nerdtree" },
+        -- current_only = true,
+        -- base = "buffer",
+        -- column = 80,
+        -- signs_on_startup = { "all" },
+        -- diagnostics_severities = { vim.diagnostic.severity.ERROR },
+      })
+    end,
+  },
+  {
+    "kevinhwang91/nvim-hlslens",
+    config = function()
+      -- require('hlslens').setup() is not required
+      require("scrollbar.handlers.search").setup({
+        -- hlslens config overrides
+      })
+    end,
+  },
+}
