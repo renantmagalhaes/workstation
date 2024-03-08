71261a157a752c9691a3813c96ecd5746b80c2c9
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-08 22:51:55 +0000

    kde for opensuse

--- utils/os-selector/os-selector.sh
+++ utils/os-selector/os-selector.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 # Select DEB or RPM system
-gnome_check=`env |grep XDG_CURRENT_DESKTOP |grep -ioh "GNOME" | awk '{print tolower($0)}'`
-kde_check=`env |grep XDG_CURRENT_DESKTOP |grep -ioh "KDE" | awk '{print tolower($0)}'`
-macos_check=`uname -a |awk '{print $1}' | awk '{print tolower($0)}'`
-linux_check=`uname -a |awk '{print $1}' | awk '{print tolower($0)}'`
+gnome_check=$(env | grep XDG_CURRENT_DESKTOP | grep -ioh "GNOME" | awk '{print tolower($0)}')
+kde_check=$(env | grep XDG_CURRENT_DESKTOP | grep -ioh "KDE" | awk '{print tolower($0)}')
+macos_check=$(uname -a | awk '{print $1}' | awk '{print tolower($0)}')
+linux_check=$(uname -a | awk '{print $1}' | awk '{print tolower($0)}')
 
 # check cmd function
 check_cmd() {
-    command -v "$1" 2> /dev/null
+	command -v "$1" 2>/dev/null
 }
 
 if check_cmd apt-get; then # FOR DEB SYSTEMS
-    if [[ $gnome_check == "gnome" ]]; then
-    bash desktop/0-DEB/0-gnome-system.sh
-    elif [[ $kde_check == "kde" ]]; then
-    echo "KDE not supported"
-    else
-    echo "Not able to identify desktop environment"
-    fi
-elif check_cmd dnf; then  # FOR RPM SYSTEMS
-    echo "System not supported"
-elif check_cmd zypper; then  # FOR OPENSUSE SYSTEMS
-    if [[ $gnome_check == "gnome" ]]; then
-    bash desktop/2-ZYPPER/2-gnome-system.sh
-    elif [[ $kde_check == "kde" ]]; then
-    echo "KDE not supported"
-    else
-    echo "Not able to identify desktop environment"
-    fi
-elif check_cmd pacman; then  # FOR ARCH SYSTEMS
-    echo "System not supported"
-elif check_cmd sw_vers; then  # FOR MACOS SYSTEMS
-    if [[ $macos_check == "darwin" ]]; then
-    bash desktop/99-MACOS/macos.sh
-    else
-    echo "Not able to identify desktop environment"
-    fi
+	if [[ $gnome_check == "gnome" ]]; then
+		bash desktop/0-DEB/0-gnome-system.sh
+	elif [[ $kde_check == "kde" ]]; then
+		echo "KDE not supported"
+	else
+		echo "Not able to identify desktop environment"
+	fi
+elif check_cmd dnf; then # FOR RPM SYSTEMS
+	echo "System not supported"
+elif check_cmd zypper; then # FOR OPENSUSE SYSTEMS
+	if [[ $gnome_check == "gnome" ]]; then
+		bash desktop/2-ZYPPER/2-gnome-system.sh
+	elif [[ $kde_check == "kde" ]]; then
+		bash desktop/2-ZYPPER/2-kde-system.sh
+	else
+		echo "Not able to identify desktop environment"
+	fi
+elif check_cmd pacman; then # FOR ARCH SYSTEMS
+	echo "System not supported"
+elif check_cmd sw_vers; then # FOR MACOS SYSTEMS
+	if [[ $macos_check == "darwin" ]]; then
+		bash desktop/99-MACOS/macos.sh
+	else
+		echo "Not able to identify desktop environment"
+	fi
 fi
