08b83268def2fb62d92e34a287da3eb928e5949b
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-09 22:42:17 +0000

    bg color

--- desktop/source/any/config/nvim/lua/plugins/notify.lua
+++ desktop/source/any/config/nvim/lua/plugins/notify.lua
@@ -3,7 +3,7 @@ return {
     "rcarriga/nvim-notify",
     config = function()
       require("notify").setup({
-        background_colour = "#2c627f",
+        background_colour = "#11121d",
       })
     end,
   },
