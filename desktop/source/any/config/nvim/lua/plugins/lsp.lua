e056cbef53ba83ef8156649030f6e8840f754e5a
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-11 19:45:05 +0000

    fix tab behavior

--- desktop/source/any/config/nvim/lua/plugins/lsp.lua
+++ desktop/source/any/config/nvim/lua/plugins/lsp.lua
@@ -44,21 +44,14 @@ return {
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-              feedkey("<Plug>(vsnip-expand-or-jump)", "")
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-            end
-          end, { "i", "s" }),
+              local entries = cmp.get_entries()
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
 
-          ["<S-Tab>"] = cmp.mapping(function()
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-              feedkey("<Plug>(vsnip-jump-prev)", "")
+              if #entries == 1 then
+                cmp.confirm()
+              end
+            else
+              fallback()
             end
           end, { "i", "s" }),
         }),
