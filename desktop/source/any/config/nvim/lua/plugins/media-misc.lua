efc3630c6070a0aa415c316fb4ef836aaf67d5b1
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-12 10:46:42 +0100

    img clip plugin

--- desktop/source/any/config/nvim/lua/plugins/media-misc.lua
+++ desktop/source/any/config/nvim/lua/plugins/media-misc.lua
@@ -33,4 +33,16 @@ return {
       })
     end,
   },
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "BufEnter",
+    opts = {
+      -- add options here
+      -- or leave it empty to use the default settings
+    },
+    keys = {
+      -- suggested keymap
+      { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+    },
+  },
 }
