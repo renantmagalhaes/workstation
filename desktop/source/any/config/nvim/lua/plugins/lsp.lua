d08b27ad4fbc10586748cd38d43135c85235f73b
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-13 12:20:39 +0100

    new plugins

--- desktop/source/any/config/nvim/lua/plugins/lsp.lua
+++ desktop/source/any/config/nvim/lua/plugins/lsp.lua
@@ -149,4 +149,14 @@ return {
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
 }
