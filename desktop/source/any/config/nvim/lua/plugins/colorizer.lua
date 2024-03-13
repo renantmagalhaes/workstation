21f95052150073dd6205e8c82fe2fac9d164e06e
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-13 12:02:03 +0100

    rainbow pkg

--- desktop/source/any/config/nvim/lua/plugins/colorizer.lua
+++ desktop/source/any/config/nvim/lua/plugins/colorizer.lua
@@ -2,7 +2,13 @@ return {
   {
     "NvChad/nvim-colorizer.lua",
     config = function()
-      require("colorizer").setup()
+      require("colorizer").setup({})
+    end,
+  },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      require("rainbow-delimiters.setup").setup({})
     end,
   },
 }
---
+++ desktop/source/any/config/nvim/lua/plugins/status-bar.lua
@@ -0,0 +1,51 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "catppuccin",
+      },
+    })
+  end,
+  -- {
+  --   "sontungexpt/sttusline",
+  --   dependencies = {
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --   event = { "BufEnter" },
+  --   config = function(_, opts)
+  --     require("sttusline").setup({
+  --       -- statusline_color = "#000000",
+  --       statusline_color = "StatusLine",
+  --
+  --       -- | 1 | 2 | 3
+  --       -- recommended: 3
+  --       laststatus = 3,
+  --       disabled = {
+  --         filetypes = {
+  --           -- "NvimTree",
+  --           -- "lazy",
+  --         },
+  --         buftypes = {
+  --           -- "terminal",
+  --         },
+  --       },
+  --       components = {
+  --         "mode",
+  --         "filename",
+  --         "git-branch",
+  --         "git-diff",
+  --         "%=",
+  --         "diagnostics",
+  --         "lsps-formatters",
+  --         "copilot",
+  --         "indent",
+  --         "encoding",
+  --         "pos-cursor",
+  --         "pos-cursor-progress",
+  --       },
+  --     })
+  --   end,
+  -- },
+}
