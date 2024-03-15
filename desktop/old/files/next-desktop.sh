ce79f4399fb923e25fa57b865107e6c454de452f
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-15 20:08:10 +0000

    move dotfiles

--- desktop/old/files/next-desktop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mon=`bspc query -D -d focused --names | cut -c1` && bspc desktop --focus $((mon)) && bspc desktop --focus $((mon + 1))$((mon + 1)) && bspc desktop --focus $((mon + 1))
