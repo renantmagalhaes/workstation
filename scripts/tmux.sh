9ec96fa75292541fafe359721ef3d811a2635421
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-18 23:10:27 +0000

    modofy tmux syslink

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,15 +1,17 @@
-#Based in Gregory's config <https://github.com/gpakosz> 
+#Based in Gregory's config <https://github.com/gpakosz>
 
 #verify tmux
-type tmux >/dev/null 2>&1 || { echo >&2 "Install tmux before run this script "; exit 1; }
+type tmux >/dev/null 2>&1 || {
+	echo >&2 "Install tmux before run this script "
+	exit 1
+}
 
 #install tmp (tmux plugin manager)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-
-#Create syslinks 
-ln -s -f $PWD/tmux/tmux.conf ~/.tmux.conf
-ln -s -f $PWD/tmux/tmux.conf.local ~/.tmux.conf.local
+#Create syslinks
+ln -s -f $PWD/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s -f $PWD/dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
 
 # Install plugins
 bash -c "~/.tmux/plugins/tpm/scripts/install_plugins.sh"
@@ -17,4 +19,5 @@ bash -c "~/.tmux/plugins/tpm/scripts/install_plugins.sh"
 # catppuccin setup
 rm -rf ~/.tmux/plugins/tmux/custom
 mkdir -p ~/.tmux/plugins/tmux/
-ln -s -f  $PWD/tmux/plugins/catppuccin/custom ~/.tmux/plugins/tmux/custom
+ln -s -f $PWD/dotfiles/tmux/plugins/catppuccin/custom ~/.tmux/plugins/tmux/custom
+
