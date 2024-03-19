7012cb91883af6746c3c22b9698f6191e649362f
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-19 17:44:15 +0100

    update paths

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -3,5 +3,6 @@
 # Create folder
 mkdir -p ~/.config/alacritty/
 
+echo "$PWD alacritty.sh"
 # syslink to file
-ln -s -f $PWD/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s -f $PWD/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -1,20 +1,18 @@
-
 #!/bin/bash
 
 # check cmd function
 check_cmd() {
-    command -v "$1" 2> /dev/null
+	command -v "$1" 2>/dev/null
 }
 
 # Add the repository key with either wget or curl
 if check_cmd apt-get; then # FOR DEB SYSTEMS
-    echo "WiP"
-elif check_cmd zypper; then  # FOR zypper SYSTEMS
-  ## Main pkgs
-  sudo zypper in hyprland -y
+	echo "WiP"
+elif check_cmd zypper; then # FOR zypper SYSTEMS
+	## Main pkgs
+	sudo zypper in hyprland -y
 
 else
-    echo "Not able to identify the system"
-    exit 0
+	echo "Not able to identify the system"
+	exit 0
 fi
-
