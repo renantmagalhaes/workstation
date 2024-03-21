234cef2ccd32ebd62359a848018aa581901ec907
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-21 17:57:22 +0000

    remove softwares not used

--- scripts/fonts-macos.sh
+++ scripts/fonts-macos.sh
@@ -4,9 +4,5 @@ brew install --cask font-cascadia-code
 brew install --cask font-cascadia-code-pl
 brew install --cask font-cascadia-mono
 brew install --cask font-cascadia-mono-pl
-
-
 brew install --cask font-hack-nerd-font
 brew install --cask font-fira-code
-
-
--- scripts/kde-themes.sh
+++ scripts/kde-themes.sh
@@ -4,22 +4,22 @@
 # Materia KDE
 wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/materia-kde/master/install.sh | sh
 
-# Orchis
-git clone https://github.com/vinceliuice/Orchis-kde.git ~/GIT-REPOS/CORE/Orchis-kde
-sh -c "~/GIT-REPOS/CORE/Orchis-kde/install.sh"
+# # Orchis
+# git clone https://github.com/vinceliuice/Orchis-kde.git ~/GIT-REPOS/CORE/Orchis-kde
+# sh -c "~/GIT-REPOS/CORE/Orchis-kde/install.sh"
+#
+# # ChromeOS
+# git clone https://github.com/vinceliuice/ChromeOS-kde.git ~/GIT-REPOS/CORE/ChromeOS-kde
+# sh -c "~/GIT-REPOS/CORE/ChromeOS-kde/install.sh"
+#
+#
+# # WhiteSur
+# git clone https://github.com/vinceliuice/WhiteSur-kde.git ~/GIT-REPOS/CORE/WhiteSur-kde
+# sh -c "~/GIT-REPOS/CORE/WhiteSur-kde/install.sh"
 
-# ChromeOS
-git clone https://github.com/vinceliuice/ChromeOS-kde.git ~/GIT-REPOS/CORE/ChromeOS-kde
-sh -c "~/GIT-REPOS/CORE/ChromeOS-kde/install.sh"
-
-
-# WhiteSur
-git clone https://github.com/vinceliuice/WhiteSur-kde.git ~/GIT-REPOS/CORE/WhiteSur-kde
-sh -c "~/GIT-REPOS/CORE/WhiteSur-kde/install.sh"
-
-# Layan Theme
-git clone https://github.com/vinceliuice/Layan-kde.git ~/GIT-REPOS/CORE/Layan-kde
-sh -c "~/GIT-REPOS/CORE/Layan-kde/install.sh"
+# # Layan Theme
+# git clone https://github.com/vinceliuice/Layan-kde.git ~/GIT-REPOS/CORE/Layan-kde
+# sh -c "~/GIT-REPOS/CORE/Layan-kde/install.sh"
 
 # Graphite Theme
 git clone https://github.com/vinceliuice/Graphite-kde-theme.git ~/GIT-REPOS/CORE/Graphite-kde-theme
