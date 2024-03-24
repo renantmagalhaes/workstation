6b2538dee02c4d70ac193151e3832af1d613bf89
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-24 21:50:48 +0000

    x11 wsl test

--- OperatingSystem/wsl-opensuse.sh
+++ OperatingSystem/wsl-opensuse.sh
@@ -21,7 +21,7 @@ sudo zypper install -y ntp
 sudo ntpdate pool.ntp.org
 
 # Install the packages from repo
-sudo zypper install -y zsh vim curl net-tools iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak neofetch evince jq firefox net-tools-deprecated busybox-sysvinit-tools bind-utils gcc-c++ rsync sassc gawk bc cron golang npm fd
+sudo zypper install -y zsh vim curl net-tools iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak neofetch evince jq firefox bind-utils gcc-c++ rsync sassc gawk bc cron golang npm fd
 
 # Install SNAP
 wsl_thumbleweed_check=$(env | grep WSL | grep -ioh "openSUSE-Tumbleweed" | awk '{print tolower($0)}')
@@ -90,11 +90,11 @@ mkdir -p ~/GIT-REPOS/CORE
 # Fix systemd init
 sudo ln -s /usr/lib/systemd/systemd /sbin/init
 
-# Enable Systemd
-sudo bash -c 'cat << EOF > /etc/wsl.conf
-[boot]
-systemd=true
-EOF'
+# # Enable Systemd
+# sudo bash -c 'cat << EOF > /etc/wsl.conf
+# [boot]
+# systemd=true
+# EOF'
 
 # WSL config
 sudo bash -c 'cat << EOF > /mnt/c/Users/renan/.wslconfig
