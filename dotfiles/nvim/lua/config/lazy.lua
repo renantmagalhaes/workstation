73eaa1dfe576e058da9a9dbd85118cd9d31199ea
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-24 20:14:32 +0000

    temp xclip solution

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -6,6 +6,23 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
+-- -- Fix windows clipboard
+-- local clip = "/mnt/c/Windows/System32/clip.exe" -- Change this path if needed
+--
+-- if vim.fn.executable(clip) then
+--   local opts = {
+--     callback = function()
+--       if vim.v.event.operator ~= "y" then
+--         return
+--       end
+--       vim.fn.system(clip, vim.fn.getreg(0))
+--     end,
+--   }
+--
+--   opts.group = vim.api.nvim_create_augroup("WSLYank", {})
+--   vim.api.nvim_create_autocmd("TextYankPost", { group = opts.group, callback = opts.callback })
+-- end
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
