27b403626d844ea780ff036b5db46e58580b3a74
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-24 19:23:40 +0000

    add fd

--- OperatingSystem/2-opensuse-kde-system.sh
+++ OperatingSystem/2-opensuse-kde-system.sh
@@ -54,7 +54,7 @@ sudo zypper install -y opi
 sudo zypper install -y kvantum-qt5 kvantum-manager kvantum-themes
 
 # Install the packages from suse repo
-sudo zypper --non-interactive install -y zsh vlc clementine breeze6-cursors vim nmap blender brasero gparted wireshark tmux curl vpnc git htop meld openvpn guake python3-pip gtk2-engines krita audacity filezilla tree remmina nload pwgen sysstat alacarte fzf ffmpeg neofetch xclip flameshot unrar gawk net-tools coreutils ncdu whois piper openssl gnome-keyring chrome-gnome-shell telnet openssh materia-gtk-theme alacritty scrot libstdc++-devel glibc-static net-tools-deprecated xprop wmctrl xdotool gcc-c++ lsd sassc virtualbox jq rsync sassc gawk bc cron golang npm
+sudo zypper --non-interactive install -y zsh vlc clementine breeze6-cursors vim nmap blender brasero gparted wireshark tmux curl vpnc git htop meld openvpn guake python3-pip gtk2-engines krita audacity filezilla tree remmina nload pwgen sysstat alacarte fzf ffmpeg neofetch xclip flameshot unrar gawk net-tools coreutils ncdu whois piper openssl gnome-keyring chrome-gnome-shell telnet openssh materia-gtk-theme alacritty scrot libstdc++-devel glibc-static net-tools-deprecated xprop wmctrl xdotool gcc-c++ lsd sassc virtualbox jq rsync sassc gawk bc cron golang npm fd
 
 # virtualbox user
 sudo usermod -aG vboxusers $USER
