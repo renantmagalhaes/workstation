6e3db1e7d4502ad67f8953c622736bc0379dd51d
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-26 16:12:37 +0100

    set width

--- dotfiles/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/nvim/lua/plugins/neo-tree.lua
@@ -8,26 +8,26 @@ return {
   },
   config = function()
     require("neo-tree").setup({
-      close_if_last_window = true, -- This is optional, adjust as needed
+      close_if_last_window = true, -- optional, adjust as needed
       -- Add filesystem component configuration
       filesystem = {
         filtered_items = {
           visible = true, -- Make filtered items visible
           hide_dotfiles = false, -- Show hidden files (dotfiles)
           hide_gitignored = false, -- Optionally, show gitignored files
-          hide_by_name = {
-            -- Specify any filenames you might want to keep hidden; empty if none
-          },
+          -- Add any specific files or folders you want to hide; empty if none
         },
       },
-      default_component_configs = {
-        -- Component configurations; add if you have specific component configs
+      -- Configure the window options here
+      window = {
+        width = 25, -- Set the width of the NeoTree window
+        -- You can also set 'position = "left"' or 'position = "right"' if you want to specify which side of the screen NeoTree opens on
       },
     })
-    -- The rest of your key mappings and commands
-    vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+    -- Custom keymaps
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-    vim.cmd[[
+    vim.cmd [[
       autocmd VimEnter * Neotree source=filesystem reveal=true
     ]]
   end,
