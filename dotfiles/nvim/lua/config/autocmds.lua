2f2eb938f05f5f0dbc2e0009717a9fce06870877
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-26 18:24:07 +0100

    add ssh config support

--- dotfiles/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = vim.fn.expand("~/.ssh/config"),
+  callback = function()
+    vim.bo.filetype = "sshconfig"
+  end,
+})
+
