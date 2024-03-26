fa7837e3dc19e274a0e2e11d818e997d216bdc2b
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-03-26 12:13:11 +0100

    view hidden files and folders

--- dotfiles/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/nvim/lua/plugins/neo-tree.lua
@@ -7,10 +7,29 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true, -- This is optional, adjust as needed
+      -- Add filesystem component configuration
+      filesystem = {
+        filtered_items = {
+          visible = true, -- Make filtered items visible
+          hide_dotfiles = false, -- Show hidden files (dotfiles)
+          hide_gitignored = false, -- Optionally, show gitignored files
+          hide_by_name = {
+            -- Specify any filenames you might want to keep hidden; empty if none
+          },
+        },
+      },
+      default_component_configs = {
+        -- Component configurations; add if you have specific component configs
+      },
+    })
+    -- The rest of your key mappings and commands
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-     vim.cmd [[
-        autocmd VimEnter * Neotree source=filesystem reveal=true
-      ]]
+    vim.cmd[[
+      autocmd VimEnter * Neotree source=filesystem reveal=true
+    ]]
   end,
 }
+
