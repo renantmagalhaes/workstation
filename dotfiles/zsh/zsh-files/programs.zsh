5d293da134ec68b25f6de2a040730186125d14e8
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-04-04 12:20:03 +0200

    eldritch theme

--- dotfiles/zsh/zsh-files/programs.zsh
+++ dotfiles/zsh/zsh-files/programs.zsh
@@ -13,6 +13,7 @@ linux_check=`uname -a |awk '{print $1}' | awk '{print tolower($0)}'`
 export FZF_DEFAULT_OPTS='--height 50% --inline-info --border --color=fg:#f8f8f2,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#9cf598,prompt:#7499f7,pointer:#ffb86c --color=marker:#ffb86c,spinner:#9cf598,header:#6272a4'
 ## with opaque bg
 #export FZF_DEFAULT_OPTS='--height 40% --inline-info --border --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+#export FZF_DEFAULT_OPTS='--color=fg:#ebfafa,bg:#282a36,hl:#37f499 --color=fg+:#ebfafa,bg+:#212337,hl+:#37f499 --color=info:#f7c67f,prompt:#04d1f9,pointer:#7081d0 --color=marker:#7081d0,spinner:#f7c67f,header:#323449'
 
 #NIX
 export NIXPKGS_ALLOW_UNFREE=1
