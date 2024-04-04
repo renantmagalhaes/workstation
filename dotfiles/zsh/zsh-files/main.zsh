4d9e2d23bfb5370408104894b53824df70e110db
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-04-04 17:19:43 +0200

    fzf-tab completion

--- dotfiles/zsh/zsh-files/main.zsh
+++ dotfiles/zsh/zsh-files/main.zsh
@@ -113,8 +113,22 @@ autoload -U +X bashcompinit && bashcompinit
 
 
 # fzf-tab
+# For tmux 
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
 zstyle ':fzf-tab:*' prefix '·'
+## disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+## set descriptions format to enable group support
+## NOTE: don't use escape sequences here, fzf-tab will ignore them
+zstyle ':completion:*:descriptions' format '[%d]'
+## set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+## force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
+## preview directory's content with eza when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+## switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group '<' '>'
 zstyle ':completion:*:descriptions' format
 
 # Additional paths
