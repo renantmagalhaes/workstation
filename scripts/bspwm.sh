6f1e29d023ec60badfaa4212682162889939ed5a
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-08-21 21:33:40 +0100

    rollback to default picom with animations

--- scripts/bspwm.sh
+++ scripts/bspwm.sh
@@ -23,7 +23,7 @@ if check_cmd apt-get; then # FOR DEB SYSTEMS
 
 	# Picom
   sudo apt-get install -y libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
-	git clone https://github.com/FT-Labs/picom.git ~/GIT-REPOS/CORE/picom
+	git clone https://github.com/yshui/picom.git ~/GIT-REPOS/CORE/picom
 	cd ~/GIT-REPOS/CORE/picom
 	meson --buildtype=release . build
 	ninja -C build
@@ -37,7 +37,7 @@ elif check_cmd zypper; then # FOR RPM SYSTEMS
 
 	# Picom
 	sudo zypper install -y dbus-1-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb1 libXext-devel libxcb-devel Mesa-libGL-devel meson pcre-devel libpixman-1-0-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorgproto-devel libepoxy-devel Mesa-libEGL-devel xcb-util-devel
-	git clone https://github.com/FT-Labs/picom.git ~/GIT-REPOS/CORE/picom
+	git clone https://github.com/yshui/picom.git ~/GIT-REPOS/CORE/picom
 	cd ~/GIT-REPOS/CORE/picom
 	meson --buildtype=release . build
 	ninja -C build
