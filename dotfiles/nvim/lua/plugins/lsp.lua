9c3efdfecb47c95e0824039ac44a1b0f8e09bd12
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-09-11 22:29:10 +0200

    sync

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,219 +1,219 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-path",
-    },
-    config = function()
-      local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              local entries = cmp.get_entries()
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-
-              if #entries == 1 then
-                cmp.confirm()
-              end
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
-          { name = "path" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "ansiblels",
-          "bashls",
-          "dockerls",
-          "docker_compose_language_service",
-          "eslint",
-          "gopls",
-          "html",
-          "jsonls",
-          "marksman",
-          "pyright",
-          "sqls",
-          "yamlls",
-          "tflint",
-        },
-      })
-    end,
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      local lspconfig = require("lspconfig")
-
-      lspconfig.tflint.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.sqls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.marksman.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.eslint.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.docker_compose_language_service.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ansiblels.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.dockerls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.docker_compose_language_service.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.yamlls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end,
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
-    config = function()
-      require("mason-null-ls").setup({
-        ensure_installed = {
-          "stylua",
-          "jq",
-          "shfmt",
-          "shellcheck",
-          "black",
-          "ruff",
-          "yq",
-          "tflint",
-          "sqlfluff",
-          "sqlfmt",
-          "markdownlint",
-          "cbfmt",
-          -- "snyk",
-        },
-      })
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        ["python"] = { { "ruff", "black" } },
-        ["markdown"] = { { "markdownlint", "cbfmt" } },
-        ["yaml"] = { "yq" },
-        ["json"] = { "jq" },
-        ["terraform"] = { "tflint" },
-        ["sql"] = { { "sqlfluff", "sqlfmt" } },
-        ["bash"] = { { "shellcheck", "shfmt" } },
-        ["sh"] = { { "shellcheck", "shfmt" } },
-      },
-    },
-  },
+  --   {
+  --     "hrsh7th/cmp-nvim-lsp",
+  --   },
+  --   {
+  --     "L3MON4D3/LuaSnip",
+  --     dependencies = {
+  --       "saadparwaiz1/cmp_luasnip",
+  --       "rafamadriz/friendly-snippets",
+  --     },
+  --   },
+  --   {
+  --     "hrsh7th/nvim-cmp",
+  --     dependencies = { "hrsh7th/cmp-emoji" },
+  --     ---@param opts cmp.ConfigSchema
+  --     opts = function(_, opts)
+  --       table.insert(opts.sources, { name = "emoji" })
+  --     end,
+  --   },
+  --   {
+  --     "hrsh7th/nvim-cmp",
+  --     dependencies = {
+  --       "hrsh7th/cmp-path",
+  --     },
+  --     config = function()
+  --       local cmp = require("cmp")
+  --       require("luasnip.loaders.from_vscode").lazy_load()
+  --
+  --       cmp.setup({
+  --         snippet = {
+  --           expand = function(args)
+  --             require("luasnip").lsp_expand(args.body)
+  --           end,
+  --         },
+  --         window = {
+  --           completion = cmp.config.window.bordered(),
+  --           documentation = cmp.config.window.bordered(),
+  --         },
+  --         mapping = cmp.mapping.preset.insert({
+  --           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+  --           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  --           ["<C-Space>"] = cmp.mapping.complete(),
+  --           ["<C-e>"] = cmp.mapping.abort(),
+  --           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  --           ["<Tab>"] = cmp.mapping(function(fallback)
+  --             if cmp.visible() then
+  --               local entries = cmp.get_entries()
+  --               cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+  --
+  --               if #entries == 1 then
+  --                 cmp.confirm()
+  --               end
+  --             else
+  --               fallback()
+  --             end
+  --           end, { "i", "s" }),
+  --         }),
+  --         sources = cmp.config.sources({
+  --           { name = "nvim_lsp" },
+  --           { name = "luasnip" }, -- For luasnip users.
+  --           { name = "path" },
+  --         }, {
+  --           { name = "buffer" },
+  --         }),
+  --       })
+  --     end,
+  --   },
+  --   {
+  --     "williamboman/mason.nvim",
+  --     lazy = false,
+  --     config = function()
+  --       require("mason").setup()
+  --     end,
+  --   },
+  --   {
+  --     "williamboman/mason-lspconfig.nvim",
+  --     config = function()
+  --       require("mason-lspconfig").setup({
+  --         ensure_installed = {
+  --           "lua_ls",
+  --           "tsserver",
+  --           "ansiblels",
+  --           "bashls",
+  --           "dockerls",
+  --           "docker_compose_language_service",
+  --           "eslint",
+  --           "gopls",
+  --           "html",
+  --           "jsonls",
+  --           "marksman",
+  --           "pyright",
+  --           "sqls",
+  --           "yamlls",
+  --           "tflint",
+  --         },
+  --       })
+  --     end,
+  --     lazy = false,
+  --     opts = {
+  --       auto_install = true,
+  --     },
+  --   },
+  --   {
+  --     "neovim/nvim-lspconfig",
+  --     lazy = false,
+  --     config = function()
+  --       local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  --
+  --       local lspconfig = require("lspconfig")
+  --
+  --       lspconfig.tflint.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.sqls.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.marksman.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.jsonls.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.eslint.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.docker_compose_language_service.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.tsserver.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.html.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.lua_ls.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.ansiblels.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.dockerls.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.bashls.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.docker_compose_language_service.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.pyright.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.yamlls.setup({
+  --         capabilities = capabilities,
+  --       })
+  --       lspconfig.gopls.setup({
+  --         capabilities = capabilities,
+  --       })
+  --
+  --       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+  --       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+  --       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+  --       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+  --     end,
+  --   },
+  --   {
+  --     "kylechui/nvim-surround",
+  --     version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  --     event = "VeryLazy",
+  --     config = function()
+  --       require("nvim-surround").setup({
+  --         -- Configuration here, or leave empty to use defaults
+  --       })
+  --     end,
+  --   },
+  --   {
+  --     "jay-babu/mason-null-ls.nvim",
+  --     event = { "BufReadPre", "BufNewFile" },
+  --     dependencies = {
+  --       "williamboman/mason.nvim",
+  --       "nvimtools/none-ls.nvim",
+  --     },
+  --     config = function()
+  --       require("mason-null-ls").setup({
+  --         ensure_installed = {
+  --           "stylua",
+  --           "jq",
+  --           "shfmt",
+  --           "shellcheck",
+  --           "black",
+  --           "ruff",
+  --           "yq",
+  --           "tflint",
+  --           "sqlfluff",
+  --           "sqlfmt",
+  --           "markdownlint",
+  --           "cbfmt",
+  --           -- "snyk",
+  --         },
+  --       })
+  --     end,
+  --   },
+  --   {
+  --     "stevearc/conform.nvim",
+  --     optional = true,
+  --     opts = {
+  --       formatters_by_ft = {
+  --         ["python"] = { { "ruff", "black" } },
+  --         ["markdown"] = { { "markdownlint", "cbfmt" } },
+  --         ["yaml"] = { "yq" },
+  --         ["json"] = { "jq" },
+  --         ["terraform"] = { "tflint" },
+  --         ["sql"] = { { "sqlfluff", "sqlfmt" } },
+  --         ["bash"] = { { "shellcheck", "shfmt" } },
+  --         ["sh"] = { { "shellcheck", "shfmt" } },
+  --       },
+  --     },
+  --   },
 }
