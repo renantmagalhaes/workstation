440cdb334fb7dc3245638cd124890085ea33f600
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-09-11 22:41:56 +0100

    update coding IDE setup

--- dotfiles/nvim/lua/plugins/markdown.lua
+++ dotfiles/nvim/lua/plugins/markdown.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   ft = { "markdown" },
+  --   build = function()
+  --     vim.fn["mkdp#util#install"]()
+  --   end,
+  -- },
   {
     "jghauser/follow-md-links.nvim",
     vim.keymap.set("n", "<bs>", ":edit #<cr>", { silent = true }),
