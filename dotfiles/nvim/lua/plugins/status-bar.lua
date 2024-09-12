2e449cf8d4d18c1141a6de3bd2bce28c2eec6d08
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-09-12 23:15:18 +0100

    lualine on the top

--- dotfiles/nvim/lua/plugins/status-bar.lua
+++ dotfiles/nvim/lua/plugins/status-bar.lua
@@ -17,23 +17,17 @@ return {
         options = {
           theme = "catppuccin",
         },
-      })
-    end,
-    opts = function(_, opts)
-      local trouble = require("trouble")
-      local symbols = trouble.statusline({
-        mode = "lsp_document_symbols",
-        groups = {},
-        title = false,
-        filter = { range = true },
-        format = "{kind_icon}{symbol.name:Normal}",
-        -- The following line is needed to fix the background color
-        -- Set it to the lualine section you want to use
-        hl_group = "lualine_c_normal",
-      })
-      table.insert(opts.sections.lualine_c, {
-        symbols.get,
-        cond = symbols.has,
+        -- Define the winbar to display lualine at the top
+        winbar = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" }, -- Git diff and LSP diagnostics
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        -- Disable the default statusline at the bottom
+        sections = {},
       })
     end,
   },
