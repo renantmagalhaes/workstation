048a8cfdb197d77e12e568ff2a13ffc3764f7d51
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-09-13 11:10:34 +0200

    preview build and fix

--- dotfiles/nvim/lua/plugins/markdown.lua
+++ dotfiles/nvim/lua/plugins/markdown.lua
@@ -1,12 +1,23 @@
 return {
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --   ft = { "markdown" },
-  --   build = function()
-  --     vim.fn["mkdp#util#install"]()
-  --   end,
-  -- },
+  {
+    -- Install markdown preview, use npx if available.
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function(plugin)
+      if vim.fn.executable("npx") then
+        vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+      else
+        vim.cmd([[Lazy load markdown-preview.nvim]])
+        vim.fn["mkdp#util#install"]()
+      end
+    end,
+    init = function()
+      if vim.fn.executable("npx") then
+        vim.g.mkdp_filetypes = { "markdown" }
+      end
+    end,
+  },
   {
     "jghauser/follow-md-links.nvim",
     vim.keymap.set("n", "<bs>", ":edit #<cr>", { silent = true }),
