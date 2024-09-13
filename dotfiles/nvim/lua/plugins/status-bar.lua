71e80574bd3e9a8304b914f524fb4e7e05c587ad
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-09-13 15:15:29 +0200

    add incline, for the future

--- dotfiles/nvim/lua/plugins/status-bar.lua
+++ dotfiles/nvim/lua/plugins/status-bar.lua
@@ -77,4 +77,12 @@ return {
   --     })
   --   end,
   -- },
+  -- {
+  --   "b0o/incline.nvim",
+  --   config = function()
+  --     require("incline").setup()
+  --   end,
+  --   -- Optional: Lazy load Incline
+  --   event = "VeryLazy",
+  -- },
 }
