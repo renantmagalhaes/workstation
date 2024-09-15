1c15770467cdf7c5e7cd9a9223908b2294a90647
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-09-15 17:06:30 +0100

    change display format

--- dotfiles/tmux/plugins/nordvpn/status.sh
+++ dotfiles/tmux/plugins/nordvpn/status.sh
@@ -2,19 +2,15 @@
 
 #if [[ `nordvpn status |grep Status |awk '{print $4}'` == "Disconnected" ]] ; then echo "#[fg=red]VPN Disconnected:" ; else echo "#[fg=blue]VPN Connected:" ; fi
 
-nordStatus=`nordvpn status | sed -n -e 's/Status: \(.*\)/\1/p'`
-nordCountry=`nordvpn status | sed -n -e 's/Country: \(.*\)/\1/p'`
+nordStatus=$(nordvpn status | sed -n -e 's/Status: \(.*\)/\1/p')
+nordCountry=$(nordvpn status | sed -n -e 's/Country: \(.*\)/\1/p')
 
-
-if [[ $nordStatus == *"Disconnected"* ]]
-then
-    echo "#[fg=red]VPN Disconnected #[fg=white]|"
-elif [[ $nordStatus == *"Connected"* ]]
-then
-    echo "#[fg=green]VPN Connected [$nordCountry] #[fg=white]|"
-elif [[ $nordStatus == *"Connecting"* ]]
-then
-    echo "#[fg=yellow]VPN Connecting |"
+if [[ $nordStatus == *"Disconnected"* ]]; then
+	echo "#[fg=red]󰒄 Disconnected #[fg=white]"
+elif [[ $nordStatus == *"Connected"* ]]; then
+	echo "#[fg=green]󰒄 Connected [$nordCountry] #[fg=white]"
+elif [[ $nordStatus == *"Connecting"* ]]; then
+	echo "#[fg=yellow]󰒄 Connecting"
 else
-    echo "NordVPN:❌ |"
+	echo "#[fg=red]󰒃 #[fg=white]"
 fi
