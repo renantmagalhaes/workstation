9c73c1bdae3a1ac26d52bca67ac739654d9913d1
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-09-19 21:53:36 +0100

    vpn cli with root

--- dotfiles/zsh/zsh-files/aliases.zsh
+++ dotfiles/zsh/zsh-files/aliases.zsh
@@ -89,8 +89,8 @@ elif [[ $linux_check == "linux" ]]; then
     alias mouse-battery='upower --dump |grep -A 5 mouse | egrep -oh '"'"'[0-9]*%'"'"''
     alias ascii-image='jp2a --output=ascii.txt --colors $1'
     alias pdf-signature='xournal'
-    alias nordvpn-c='nordvpn connect && nordvpn whitelist add subnet 192.168.2.0/24 && nordvpn set tpl on && nordvpn set killswitch on && nordvpn set autoconnect on'
-    alias nordvpn-d='nordvpn set autoconnect off && nordvpn set killswitch off && nordvpn set tpl off && nordvpn disconnect'
+    alias nordvpn-c='sudo nordvpn connect && sudo nordvpn set lan-discovery on && sudo nordvpn set tpl on && sudo nordvpn set killswitch on && sudo nordvpn set autoconnect on'
+    alias nordvpn-d='sudo nordvpn set autoconnect off && sudo nordvpn set killswitch off && sudo nordvpn set tpl off && sudo nordvpn disconnect'
 
 else
     echo "error on SO check"
