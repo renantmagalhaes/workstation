2456fb5a61352fb3415c10ceb92579763ef0ddd8
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2024-09-25 16:31:04 +0200

    lualine buble theme

--- dotfiles/nvim/lua/plugins/status-bar.lua
+++ dotfiles/nvim/lua/plugins/status-bar.lua
@@ -193,19 +193,24 @@ return {
     "nvim-lualine/lualine.nvim",
     config = function()
       local lualine = require("lualine")
+
+      -- Merge colors from both configurations
       local colors = {
         bg = "#202328",
         fg = "#bbc2cf",
         yellow = "#ECBE7B",
-        cyan = "#008080",
+        cyan = "#79dac8", -- From bubble theme
         darkblue = "#081633",
         green = "#98be65",
         orange = "#FF8800",
-        violet = "#a9a1e1",
+        violet = "#d183e8", -- From bubble theme
         magenta = "#c678dd",
-        blue = "#51afef",
-        red = "#ec5f67",
-        white = "#ffffff",
+        blue = "#80a0ff", -- From bubble theme
+        red = "#ff5189", -- From bubble theme
+        white = "#c6c6c6", -- From bubble theme
+        truewhite = "#ffffff", -- From bubble theme
+        black = "#080808", -- From bubble theme
+        grey = "#303030", -- From bubble theme
       }
 
       local conditions = {
@@ -222,209 +227,188 @@ return {
         end,
       }
 
+      -- Bubble theme configuration
+      local bubbles_theme = {
+        normal = {
+          a = { fg = colors.black, bg = colors.violet },
+          b = { fg = colors.white, bg = colors.grey },
+          c = { fg = colors.white, bg = colors.bg },
+        },
+        insert = { a = { fg = colors.black, bg = colors.blue } },
+        visual = { a = { fg = colors.black, bg = colors.cyan } },
+        replace = { a = { fg = colors.black, bg = colors.red } },
+        inactive = {
+          a = { fg = colors.white, bg = colors.black },
+          b = { fg = colors.white, bg = colors.black },
+          c = { fg = colors.white, bg = colors.bg },
+        },
+      }
+
       local config = {
         options = {
           component_separators = "",
-          section_separators = "",
-          theme = {
-            normal = { c = { fg = colors.fg, bg = colors.bg } },
-            inactive = { c = { fg = colors.fg, bg = colors.bg } },
-          },
+          section_separators = { left = "", right = "" },
+          theme = bubbles_theme,
         },
         sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_y = {},
-          lualine_z = {},
           lualine_c = {},
           lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
         },
         inactive_sections = {
-          lualine_a = {},
+          lualine_a = { "filename" },
           lualine_b = {},
-          lualine_y = {},
-          lualine_z = {},
           lualine_c = {},
           lualine_x = {},
+          lualine_y = {},
+          lualine_z = { "location" },
         },
       }
 
-      local function ins_left(component)
-        table.insert(config.sections.lualine_c, component)
-      end
+      -- Insert components into lualine sections
 
-      local function ins_right(component)
-        table.insert(config.sections.lualine_x, component)
-      end
+      -- Left section (mode)
+      config.sections.lualine_a = {
+        {
+          function()
+            -- Define the icon in one place
+            local mode_icon = ""
 
-      ins_left({
-        function()
-          return "▊"
-        end,
-        color = { fg = colors.blue },
-        padding = { left = 0, right = 1 },
-      })
+            -- Define the mode names
+            local mode_map = {
+              n = mode_icon .. " NORMAL",
+              i = mode_icon .. " INSERT",
+              v = mode_icon .. " VISUAL",
+              [""] = mode_icon .. " VISUAL BLOCK",
+              V = mode_icon .. " VISUAL LINE",
+              c = mode_icon .. " COMMAND",
+              no = mode_icon .. " NORMAL",
+              s = mode_icon .. " SELECT",
+              S = mode_icon .. " SELECT LINE",
+              [""] = mode_icon .. " SELECT BLOCK",
+              ic = mode_icon .. " INSERT COMPLETION",
+              R = mode_icon .. " REPLACE",
+              Rv = mode_icon .. " VISUAL REPLACE",
+              cv = mode_icon .. " VIM EX",
+              ce = mode_icon .. " NORMAL EX",
+              r = mode_icon .. " HIT-ENTER",
+              rm = mode_icon .. " MORE",
+              ["r?"] = mode_icon .. " CONFIRM",
+              ["!"] = mode_icon .. " SHELL",
+              t = mode_icon .. " TERMINAL",
+            }
 
-      ins_left({
-        function()
-          -- Define the icon in one place
-          local mode_icon = ""
-          -- local mode_icon = ""
-
-          -- Define the mode names
-          local mode_map = {
-            n = mode_icon .. " NORMAL",
-            i = mode_icon .. " INSERT",
-            v = mode_icon .. " VISUAL",
-            [""] = mode_icon .. " VISUAL BLOCK",
-            V = mode_icon .. " VISUAL LINE",
-            c = mode_icon .. " COMMAND",
-            no = mode_icon .. " NORMAL",
-            s = mode_icon .. " SELECT",
-            S = mode_icon .. " SELECT LINE",
-            [""] = mode_icon .. " SELECT BLOCK",
-            ic = mode_icon .. " INSERT COMPLETION",
-            R = mode_icon .. " REPLACE",
-            Rv = mode_icon .. " VISUAL REPLACE",
-            cv = mode_icon .. " VIM EX",
-            ce = mode_icon .. " NORMAL EX",
-            r = mode_icon .. " HIT-ENTER",
-            rm = mode_icon .. " MORE",
-            ["r?"] = mode_icon .. " CONFIRM",
-            ["!"] = mode_icon .. " SHELL",
-            t = mode_icon .. " TERMINAL",
-          }
-
-          -- Return the corresponding mode name with the icon
-          return mode_map[vim.fn.mode()] or (mode_icon .. " UNKNOWN")
-        end,
-        color = function()
-          local mode_color = {
-            n = colors.red,
-            i = colors.green,
-            v = colors.blue,
-            [""] = colors.blue,
-            V = colors.blue,
-            c = colors.magenta,
-            no = colors.red,
-            s = colors.orange,
-            S = colors.orange,
-            [""] = colors.orange,
-            ic = colors.yellow,
-            R = colors.violet,
-            Rv = colors.violet,
-            cv = colors.red,
-            ce = colors.red,
-            r = colors.cyan,
-            rm = colors.cyan,
-            ["r?"] = colors.cyan,
-            ["!"] = colors.red,
-            t = colors.red,
-          }
-          return { fg = mode_color[vim.fn.mode()] }
-        end,
-        padding = { right = 1 },
-      })
-
-      ins_left({
-        "filesize",
-        cond = conditions.buffer_not_empty,
-      })
-
-      -- Update the filename component to show the full path
-      ins_left({
-        "filename",
-        cond = conditions.buffer_not_empty,
-        path = 3, -- Show the relative filepath
-        color = { fg = colors.white, gui = "bold" },
-      })
-
-      ins_left({ "location" })
-      ins_left({ "progress", color = { fg = colors.fg, gui = "bold" } })
-
-      ins_left({
-        "diagnostics",
-        sources = { "nvim_diagnostic" },
-        symbols = { error = " ", warn = " ", info = " " },
-        diagnostics_color = {
-          error = { fg = colors.red },
-          warn = { fg = colors.yellow },
-          info = { fg = colors.cyan },
+            -- Return the corresponding mode name with the icon
+            return mode_map[vim.fn.mode()] or (mode_icon .. " UNKNOWN")
+          end,
+          color = function()
+            local mode_color = {
+              n = colors.red,
+              i = colors.green,
+              v = colors.blue,
+              [""] = colors.blue,
+              V = colors.blue,
+              c = colors.magenta,
+              no = colors.red,
+              s = colors.orange,
+              S = colors.orange,
+              [""] = colors.orange,
+              ic = colors.yellow,
+              R = colors.violet,
+              Rv = colors.violet,
+              cv = colors.red,
+              ce = colors.red,
+              r = colors.cyan,
+              rm = colors.cyan,
+              ["r?"] = colors.cyan,
+              ["!"] = colors.red,
+              t = colors.red,
+            }
+            return { fg = mode_color[vim.fn.mode()], bg = colors.black }
+          end,
+          separator = { left = "" },
+          right_padding = 2,
         },
-      })
+      }
 
-      ins_left({
-        function()
-          return "%="
-        end,
-      })
+      -- Left section (branch and diff)
+      config.sections.lualine_b = {
+        {
+          "branch",
+          icon = "",
+          color = { fg = colors.violet, gui = "bold" },
+        },
+        {
+          "diff",
+          symbols = { added = " ", modified = "󰝤 ", removed = " " },
+          diff_color = {
+            added = { fg = colors.green },
+            modified = { fg = colors.orange },
+            removed = { fg = colors.red },
+          },
+          cond = conditions.hide_in_width,
+        },
+      }
 
-      -- Move the LSP section to the right
-      ins_right({
-        function()
-          local msg = "No Active Lsp"
-          local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-          local clients = vim.lsp.get_active_clients()
-          if next(clients) == nil then
-            return msg
-          end
-          for _, client in ipairs(clients) do
-            local filetypes = client.config.filetypes
-            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-              return client.name
+      -- Center section (filename centered and inside bubble separators)
+      config.sections.lualine_c = {
+        { "%=" },
+        {
+          "filetype",
+          fmt = string.upper,
+          icons_enabled = true,
+          color = { fg = colors.green, bg = colors.grey, gui = "bold" },
+          separator = { left = "", right = "" },
+        },
+
+        {
+          "filename",
+          cond = conditions.buffer_not_empty,
+          path = 3, -- Show the relative filepath
+          color = { fg = colors.black, bg = colors.truewhite, gui = "bold" },
+          separator = { left = "", right = "" },
+        },
+        { "%=" },
+      }
+
+      -- Right section (LSP, filetype, progress)
+      config.sections.lualine_y = {
+        {
+          function()
+            local msg = "No Active Lsp"
+            local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+            local clients = vim.lsp.get_active_clients()
+            if next(clients) == nil then
+              return msg
             end
-          end
-          return msg
-        end,
-        icon = " LSP:",
-        color = { fg = colors.blue, gui = "bold" },
-      })
-
-      -- ins_right({
-      --   "o:encoding",
-      --   fmt = string.upper,
-      --   cond = conditions.hide_in_width,
-      --   color = { fg = colors.green, gui = "bold" },
-      -- })
-      --
-      -- ins_right({
-      --   "fileformat",
-      --   fmt = string.upper,
-      --   icons_enabled = false,
-      --   color = { fg = colors.green, gui = "bold" },
-      -- })
-
-      ins_right({
-        "filetype",
-        fmt = string.upper,
-        icons_enabled = true,
-        color = { fg = colors.green, gui = "bold" },
-      })
-
-      ins_right({
-        "branch",
-        icon = "",
-        color = { fg = colors.violet, gui = "bold" },
-      })
-
-      ins_right({
-        "diff",
-        symbols = { added = " ", modified = "󰝤 ", removed = " " },
-        diff_color = {
-          added = { fg = colors.green },
-          modified = { fg = colors.orange },
-          removed = { fg = colors.red },
+            for _, client in ipairs(clients) do
+              local filetypes = client.config.filetypes
+              if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                return client.name
+              end
+            end
+            return msg
+          end,
+          icon = " LSP:",
+          color = { fg = colors.blue, gui = "bold" },
         },
-        cond = conditions.hide_in_width,
-      })
+        {
+          "progress",
+          color = { fg = colors.fg, gui = "bold" },
+        },
+      }
 
-      ins_right({
-        function()
-          return "▊"
-        end,
-        color = { fg = colors.blue },
-        padding = { left = 1 },
-      })
+      -- Right-most section (location)
+      config.sections.lualine_z = {
+        {
+          "location",
+          separator = { right = "" },
+          left_padding = 2,
+        },
+      }
 
       lualine.setup(config)
     end,
