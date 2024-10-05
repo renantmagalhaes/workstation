786c55fe79b1999b4feed24d36835824d0d09886
RTM <rtm@insecure.codes>
2024-10-05 11:55:09 +0100

    theme tweaks

--- dotfiles/rofi/scripts/power-menu.sh
+++ dotfiles/rofi/scripts/power-menu.sh
@@ -4,21 +4,20 @@
 OPTIONS="Shutdown\nRestart\nCancel"
 
 # Show options in Rofi and capture the selection
-SELECTED_OPTION=$(echo -e "$OPTIONS" | rofi -dmenu -p "Power Menu" -i)
+SELECTED_OPTION=$(echo -e "$OPTIONS" | rofi -dmenu -p "Power Menu" -i -theme Arc-Dark -lines 3 -theme-str "window { width: 15%; } listview { lines: 3; }")
 
 # Execute the selected option
 case $SELECTED_OPTION in
-    Shutdown)
-        systemctl poweroff
-        ;;
-    Restart)
-        systemctl reboot
-        ;;
-    Cancel)
-        exit 0
-        ;;
-    *)
-        exit 1
-        ;;
+Shutdown)
+	systemctl poweroff
+	;;
+Restart)
+	systemctl reboot
+	;;
+Cancel)
+	exit 0
+	;;
+*)
+	exit 1
+	;;
 esac
-
