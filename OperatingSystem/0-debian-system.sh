0f5c76b3e8cd6665b5856347529654d6160421ed
RTM <rtm@insecure.codes>
2024-10-08 23:37:01 +0100

    floorp browser

--- OperatingSystem/0-debian-system.sh
+++ OperatingSystem/0-debian-system.sh
@@ -119,6 +119,12 @@ sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
 sudo apt-get update
 sudo apt-get install -y vivaldi-stable
 
+## Floorp Browser
+curl -fsSL https://ppa.floorp.app/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
+sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list "https://ppa.floorp.app/Floorp.list"
+sudo apt update
+sudo apt install floorp
+
 ## Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
 sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
