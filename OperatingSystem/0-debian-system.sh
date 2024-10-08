b2d15b2aee763d390d8937ff7c81f91bd6ac917a
RTM <rtm@insecure.codes>
2024-10-08 23:37:53 +0100

    auto install

--- OperatingSystem/0-debian-system.sh
+++ OperatingSystem/0-debian-system.sh
@@ -123,7 +123,7 @@ sudo apt-get install -y vivaldi-stable
 curl -fsSL https://ppa.floorp.app/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
 sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list "https://ppa.floorp.app/Floorp.list"
 sudo apt update
-sudo apt install floorp
+sudo apt install -y floorp
 
 ## Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
