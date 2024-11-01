3f04c1b41c6b8c02d31de8af4b3b691f95787382
RTM <rtm@insecure.codes>
2024-11-01 12:55:47 +0000

    add wireguard to status

--- dotfiles/tmux/plugins/nordvpn/status.sh
+++ dotfiles/tmux/plugins/nordvpn/status.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-#if [[ `nordvpn status |grep Status |awk '{print $4}'` == "Disconnected" ]] ; then echo "#[fg=red]VPN Disconnected:" ; else echo "#[fg=blue]VPN Connected:" ; fi
-
+# NordVPN status check
 nordStatus=$(nordvpn status | sed -n -e 's/Status: \(.*\)/\1/p')
-nordCountry=$(nordvpn status | sed -n -e 's/Country: \(.*\)/\1/p')
 
-if [[ $nordStatus == *"Disconnected"* ]]; then
-	echo "#[fg=#ff4237]󰒄 Disconnected #[fg=white]"
-elif [[ $nordStatus == *"Connected"* ]]; then
-	echo "#[fg=#7feaac]󰒄 Connected [$nordCountry] #[fg=white]"
+# Check for any active WireGuard interfaces
+wgInterface=$(ls /sys/class/net | grep -E '^wireguard' | head -n 1)
+
+# Determine combined VPN status
+if [[ $nordStatus == *"Connected"* || -n $wgInterface ]]; then
+	# If either NordVPN or WireGuard is connected
+	if [[ $nordStatus == *"Connected"* ]]; then
+		nordCountry=$(nordvpn status | sed -n -e 's/Country: \(.*\)/\1/p')
+		echo "#[fg=#7feaac]󰒄 VPN Connected [NordVPN: $nordCountry] #[fg=white]"
+	else
+		echo "#[fg=#7feaac]󰒄 VPN Connected [WireGuard] #[fg=white]"
+	fi
 elif [[ $nordStatus == *"Connecting"* ]]; then
-	echo "#[fg=#fef65b]󰒄 Connecting"
+	# If NordVPN is connecting (WireGuard does not have a "connecting" state)
+	echo "#[fg=#fef65b]󰒄 VPN Connecting #[fg=white]"
 else
-	echo "#[fg=#000000]󰒃 #[fg=white]"
+	# If neither is connected
+	echo "#[fg=#ff4237]󰒄 VPN Disconnected #[fg=white]"
 fi
