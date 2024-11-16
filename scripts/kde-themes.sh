0575a4d859db165d55489c235df85ee8623831d2
RTM <rtm@insecure.codes>
2024-11-16 22:15:51 +0000

    new kde theme

--- scripts/kde-themes.sh
+++ scripts/kde-themes.sh
@@ -30,8 +30,12 @@ git clone https://github.com/vinceliuice/Fluent-kde.git ~/GIT-REPOS/CORE/Fluent-
 sh -c "~/GIT-REPOS/CORE/Fluent-kde/install.sh --round"
 sh -c "~/GIT-REPOS/CORE/Fluent-kde/install.sh --round --solid"
 
+# KDE MacSequoia
+git clone https://github.com/vinceliuice/MacSequoia-kde.git ~/GIT-REPOS/CORE/MacSequoia-kde
+sh -c "~/GIT-REPOS/CORE/MacSequoia-kde/install.sh --round"
+sh -c "~/GIT-REPOS/CORE/MacSequoia-kde/sddm/install.sh"
+
 # SDDM
-# ## 
+# ##
 # wget https://github.com/renantmagalhaes/workstation/raw/static-files/sddm/sugar-candy.tar.gz -O /tmp/sugar-candy.tar.gz
 # sudo tar -xzvf /tmp/sugar-candy.tar.gz -C /usr/share/sddm/themes
-
