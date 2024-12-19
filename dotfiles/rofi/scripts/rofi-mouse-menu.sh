21651477e3e33103ec6e82d0c3298b922fae55b0
RTM <rtm@insecure.codes>
2024-12-19 22:25:17 +0000

    change menu. revisit in the future

--- dotfiles/rofi/scripts/rofi-mouse-menu.sh
+++ dotfiles/rofi/scripts/rofi-mouse-menu.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wmctrl -l | awk '{print $1, substr($0, index($0, $4))}' | rofi -dmenu -p "Open Windows" -theme Arc-Dark -matching fuzzy | awk '{print $1}' | xargs -I {} wmctrl -ia {}
+#wmctrl -l | awk '{print $1, substr($0, index($0, $4))}' | rofi -dmenu -p "Open Windows" -theme Arc-Dark -matching fuzzy | awk '{print $1}' | xargs -I {} wmctrl -ia {}
+rofi -show window -theme menu.rasi -matching fuzzy
