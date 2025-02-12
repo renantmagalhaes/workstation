11bb2fa0c67036e874c02ea85ff0f17c805d5848
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2025-02-12 17:18:09 +0100

    update theme settings

--- dotfiles/nvim/lua/plugins/themes.lua
+++ dotfiles/nvim/lua/plugins/themes.lua
@@ -102,7 +102,7 @@ return {
   --       on_colors = function(colors) end,
   --
   --       --- You can override specific highlights to use other groups or a hex color
-  --       --- function will be called with a Highlights and ColorScheme table
+  --       --- function will be called with a Highlights and ColorScheme ta /ble
   --       ---@param highlights Highlights
   --       ---@param colors ColorScheme
   --       on_highlights = function(highlights, colors) end,
@@ -115,12 +115,65 @@ return {
     priority = 1000,
     config = function()
       require("cyberdream").setup({
-        -- Recommended - see "Configuring" below for more config options
-        -- transparent = true,
+        -- Set light or dark variant
+        variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+
+        -- Enable transparent background
+        transparent = true,
+
+        -- Reduce the overall saturation of colours for a more muted look
+        saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
+
+        -- Enable italics comments
         italic_comments = true,
-        hide_fillchars = true,
-        borderless_pickers = true,
+
+        -- Replace all fillchars with ' ' for the ultimate clean look
+        hide_fillchars = false,
+
+        -- Apply a modern borderless look to pickers like Telescope, Snacks Picker & Fzf-Lua
+        borderless_pickers = false,
+
+        -- Set terminal colors used in `:terminal`
         terminal_colors = true,
+
+        -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+        cache = false,
+
+        -- Override highlight groups with your own colour values
+        highlights = {
+          -- Highlight groups to override, adding new groups is also possible
+          -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+          --
+          -- Example:
+          Comment = { fg = "#696969", bg = "NONE", italic = true },
+
+          -- More examples can be found in `lua/cyberdream/extensions/*.lua`
+        },
+
+        -- -- Override a highlight group entirely using the built-in colour palette
+        -- overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
+        --   -- Example:
+        --   return {
+        --     Comment = { fg = colors.green, bg = "NONE", italic = true },
+        --     ["@property"] = { fg = colors.magenta, bold = true },
+        --   }
+        -- end,
+
+        -- Override a color entirely
+        -- colors = {
+        --   -- For a list of colors see `lua/cyberdream/colours.lua`
+        --   -- Example:
+        --   bg = "#000000",
+        --   green = "#00ff00",
+        --   magenta = "#ff00ff",
+        -- },
+
+        -- Disable or enable colorscheme extensions
+        extensions = {
+          telescope = true,
+          notify = true,
+          mini = true,
+        },
       })
     end,
   },
