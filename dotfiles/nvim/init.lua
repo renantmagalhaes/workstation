6a3fa240d93c8283ec0f2e78d81fab754079c99d
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2025-02-12 16:55:10 +0100

    sync

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,14 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- Fix MarkdownPreview plugins from build
-spec = {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.cmd([[Lazy load markdown-preview.nvim]])
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
-}
