abc4c1ad21f664c60ac436b897485ac32db2d7aa
RTM <rtm@insecure.codes>
2025-03-01 20:23:42 +0000

    Fix markdown preview plugin

--- scripts/latest-node.sh
+++ scripts/latest-node.sh
@@ -4,6 +4,8 @@
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+# Install yarn
+sudo npm install --global yarn
 # Display installed versions
 echo "Node.js version: $(node --version)"
 echo "npm version: $(npm --version)
