d04898735b31b065e82b2c29a72f2bbe28c22d7a
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2025-03-19 13:48:17 +0100

    add ffmpeg

--- OperatingSystem/wsl-zypper.sh
+++ OperatingSystem/wsl-zypper.sh
@@ -34,7 +34,7 @@ sudo zypper install -y ntp
 sudo ntpdate pool.ntp.org
 
 # Install the packages from repo
-sudo zypper install -y zsh vim curl net-tools net-tools-deprecated iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak neofetch evince jq firefox bind-utils gcc-c++ rsync sassc gawk bc cron golang npm libcap-progs sqlite3 python312-pipx cifs-utilsq
+sudo zypper install -y zsh vim curl net-tools net-tools-deprecated iproute2 git htop meld tree nload pwgen sysstat xclip unrar unzip python3 python3-pip net-tools ncdu whois flatpak neofetch evince jq firefox bind-utils gcc-c++ rsync sassc gawk bc cron golang npm libcap-progs sqlite3 python312-pipx cifs-utilsq ffmpeg
 
 # Install yarn
 sudo npm install --global yarn
