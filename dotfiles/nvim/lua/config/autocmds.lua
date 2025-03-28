98af840df714f9b7919f3f32a10952a2f349f5d8
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2025-03-28 17:16:50 +0100

    set ssh config file to be broader

--- dotfiles/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/lua/config/autocmds.lua
@@ -1,14 +1,24 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 --
 -- Add any additional autocmds here
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = vim.fn.expand("~/.ssh/config"),
-    callback = function()
-      vim.bo.filetype = "sshconfig"
-    end,
-  })
+--
+-- vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+--   pattern = { "config", "config.d/*" },
+--   callback = function(args)
+--     -- args.file is the absolute path Neovim opened
+--     local parent = vim.fn.fnamemodify(args.file, ":h")
+--     local ssh_dir = vim.fn.expand("$HOME/.ssh")
+--     if parent == ssh_dir then
+--       vim.bo.filetype = "sshconfig"
+--     end
+--   end,
+-- })
+
+vim.filetype.add({
+  filename = {
+    ["config"] = "sshconfig",
+  },
+})
