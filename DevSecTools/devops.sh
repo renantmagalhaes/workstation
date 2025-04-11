823010ad0feacc6591431638888a2a2405d5fa67
RTM <rtm@insecure.codes>
2025-04-11 17:33:34 +0100

    terminal_syntax

--- DevSecTools/devops.sh
+++ DevSecTools/devops.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-type brew >/dev/null 2>&1 || { echo >&2 "The brew cli is required for this script to run."; exit 1; }
-
-macos_check=`uname -a |awk '{print $1}' | awk '{print tolower($0)}'`
-linux_check=`uname -a |awk '{print $1}' | awk '{print tolower($0)}'`
-
-
-check_cmd() {
-    command -v "$1" 2> /dev/null
+type brew >/dev/null 2>&1 || {
+	echo >&2 "The brew cli is required for this script to run."
+	exit 1
 }
 
+macos_check=$(uname -a | awk '{print $1}' | awk '{print tolower($0)}')
+linux_check=$(uname -a | awk '{print $1}' | awk '{print tolower($0)}')
+
+check_cmd() {
+	command -v "$1" 2>/dev/null
+}
 
 if [[ $macos_check == "darwin" ]]; then
-    # AWS CLI
-    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "/tmp/AWSCLIV2.pkg"
-    sudo installer -pkg /tmp/AWSCLIV2.pkg -target /
+	# AWS CLI
+	curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "/tmp/AWSCLIV2.pkg"
+	sudo installer -pkg /tmp/AWSCLIV2.pkg -target /
 
 elif [[ $linux_check == "linux" ]]; then
-    # AWS CLI
-    cd /tmp/
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install
+	# AWS CLI
+	cd /tmp/
+	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+	unzip awscliv2.zip
+	sudo ./aws/install
 
 else
-    echo "System not detected ... exiting"
-    exit
+	echo "System not detected ... exiting"
+	exit
 fi
 
 ## EKS
@@ -39,15 +40,17 @@ brew install weaveworks/tap/eksctl
 ### kubectl
 brew install kubectl
 
+### terraform
+brew install hashicorp/tap/terraform
 
 if check_cmd apt-get; then # FOR DEB SYSTEMS
-    neofetch
-elif check_cmd zypper; then  # FOR SUSE SYSTEMS
-    neofetch
-elif check_cmd sw_vers; then  # FOR Arch SYSTEMS
-    neofetch
-else 
-    echo "Not able to identify the system"
+	neofetch
+elif check_cmd zypper; then # FOR SUSE SYSTEMS
+	neofetch
+elif check_cmd sw_vers; then # FOR Arch SYSTEMS
+	neofetch
+else
+	echo "Not able to identify the system"
 fi
 
 #clear
