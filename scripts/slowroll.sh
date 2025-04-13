3e9e28744365a2a0dc746836a8d57d9789de9f42
RTM <rtm@insecure.codes>
2025-04-13 15:19:02 +0100

    remove packman from slowroll

--- scripts/slowroll.sh
+++ scripts/slowroll.sh
@@ -16,4 +16,4 @@ fi
 shopt -s globstar && TMPSR=$(mktemp -d) && zypper --pkg-cache-dir=${TMPSR} download openSUSE-repos-Slowroll && zypper modifyrepo --all --disable && zypper install ${TMPSR}/**/openSUSE-repos-Slowroll*.rpm && zypper dist-upgrade
 
 # Packman repo
-zypper ar --refresh -p 70 http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Slowroll/Essentials/ packman
+# zypper ar --refresh -p 70 http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Slowroll/Essentials/ packman
