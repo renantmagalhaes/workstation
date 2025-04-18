7fdf0e611d988cb851433b47de2274bab42f79f3
RTM <rtm@insecure.codes>
2025-04-18 22:57:00 +0100

    adjust time to show traybar

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -2,23 +2,23 @@
 
 # Function to start bars
 start_bars() {
-    # Terminate already running bar instances
-    pidof polybar | xargs kill -9
+	# Terminate already running bar instances
+	pidof polybar | xargs kill -9
 
-    # Wait until the processes have been shut down
-    while pgrep -x polybar >/dev/null; do sleep 1; done
+	# Wait until the processes have been shut down
+	while pgrep -x polybar >/dev/null; do sleep 1; done
 
-    # Launch bars
-    sleep 1
-    polybar main >>~/polybar.log 2>&1 &
-    polybar main-scroll >>~/polybar-scroll.log 2>&1 &
-    polybar main-scroll-bottom >>~/polybar-scroll.log 2>&1 &
-    sleep 1
-    polybar secondary >>~/polybar.log 2>&1 &
-    polybar secondary-scroll >>~/polybar-scroll.log 2>&1 &
-    polybar secondary-scroll-bottom >>~/polybar-scroll.log 2>&1 &
+	# Launch bars
+	sleep 1
+	polybar main >>~/polybar.log 2>&1 &
+	polybar main-scroll >>~/polybar-scroll.log 2>&1 &
+	polybar main-scroll-bottom >>~/polybar-scroll.log 2>&1 &
+	sleep 1
+	polybar secondary >>~/polybar.log 2>&1 &
+	polybar secondary-scroll >>~/polybar-scroll.log 2>&1 &
+	polybar secondary-scroll-bottom >>~/polybar-scroll.log 2>&1 &
 
-    echo "Bars launched..."
+	echo "Bars launched..."
 }
 
 # Start the bars initially
@@ -26,30 +26,30 @@ start_bars
 
 # Monitor bars
 while true; do
-    sleep 1
-    # Check and restart individual bars if necessary
-    if ! pgrep -fx "polybar main-scroll" >/dev/null; then
-        echo "Restarting main-scroll..."
-        polybar main-scroll >>~/polybar-scroll.log 2>&1 &
-    fi
-    if ! pgrep -fx "polybar main-scroll-bottom" >/dev/null; then
-        echo "Restarting main-scroll-bottom..."
-        polybar main-scroll-bottom >>~/polybar-scroll.log 2>&1 &
-    fi
-    if ! pgrep -fx "polybar main" >/dev/null; then
-        echo "Restarting main..."
-        polybar main >>~/polybar.log 2>&1 &
-    fi
-    if ! pgrep -fx "polybar secondary" >/dev/null; then
-        echo "Restarting secondary..."
-        polybar secondary >>~/polybar.log 2>&1 &
-    fi
-    if ! pgrep -fx "polybar secondary-scroll" >/dev/null; then
-        echo "Restarting secondary-scroll..."
-        polybar secondary-scroll >>~/polybar-scroll.log 2>&1 &
-    fi
-    if ! pgrep -fx "polybar secondary-scroll-bottom" >/dev/null; then
-        echo "Restarting secondary-scroll-bottom..."
-        polybar secondary-scroll-bottom >>~/polybar-scroll.log 2>&1 &
-    fi
+	sleep 10
+	# Check and restart individual bars if necessary
+	if ! pgrep -fx "polybar main-scroll" >/dev/null; then
+		echo "Restarting main-scroll..."
+		polybar main-scroll >>~/polybar-scroll.log 2>&1 &
+	fi
+	if ! pgrep -fx "polybar main-scroll-bottom" >/dev/null; then
+		echo "Restarting main-scroll-bottom..."
+		polybar main-scroll-bottom >>~/polybar-scroll.log 2>&1 &
+	fi
+	if ! pgrep -fx "polybar main" >/dev/null; then
+		echo "Restarting main..."
+		polybar main >>~/polybar.log 2>&1 &
+	fi
+	if ! pgrep -fx "polybar secondary" >/dev/null; then
+		echo "Restarting secondary..."
+		polybar secondary >>~/polybar.log 2>&1 &
+	fi
+	if ! pgrep -fx "polybar secondary-scroll" >/dev/null; then
+		echo "Restarting secondary-scroll..."
+		polybar secondary-scroll >>~/polybar-scroll.log 2>&1 &
+	fi
+	if ! pgrep -fx "polybar secondary-scroll-bottom" >/dev/null; then
+		echo "Restarting secondary-scroll-bottom..."
+		polybar secondary-scroll-bottom >>~/polybar-scroll.log 2>&1 &
+	fi
 done
