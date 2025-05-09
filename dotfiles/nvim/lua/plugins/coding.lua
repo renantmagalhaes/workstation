256b58d699f4bdc79c6e1ed9167d955df1f916ea
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2025-05-09 11:55:03 +0200

    add ensre installed

--- dotfiles/nvim/lua/plugins/coding.lua
+++ dotfiles/nvim/lua/plugins/coding.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "terraform-ls",
+        "tflint",
+        "tfsec",
+      },
+    },
+  },
+  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
