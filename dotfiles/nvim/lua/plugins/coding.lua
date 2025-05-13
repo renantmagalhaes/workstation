e2d0c1315c48c6af5f89c955f5d04c3989239e12
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2025-05-13 15:14:00 +0200

    remove fix mason dependency

--- dotfiles/nvim/lua/plugins/coding.lua
+++ dotfiles/nvim/lua/plugins/coding.lua
@@ -1,6 +1,6 @@
 return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  -- { "mason-org/mason.nvim", version = "^1.0.0" },
+  -- { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   {
     "williamboman/mason.nvim",
     opts = {
