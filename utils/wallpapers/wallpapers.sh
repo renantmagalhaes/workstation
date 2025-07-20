afc8dbc7f1d33d71c416f19735e36d43887e3ba0
RTM <rtm@insecure.codes>
2025-07-20 21:33:55 +0100

    cleanup 2025.07

--- utils/wallpapers/wallpapers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download wallpapers
-
-git clone git@github.com:renantmagalhaes/wallpapers.git ~/GIT-REPOS/CORE/wallpapers
-
-# Syslink with pictures folder
-ln -s -f ~/GIT-REPOS/CORE/wallpapers ~/Pictures
