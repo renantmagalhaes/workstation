cd21e2289169e7a9af9668624ab8924969a449dd
RTM <rtm@insecure.codes>
2025-08-07 00:43:04 +0100

    update settings for debian 12 install

--- scripts/kitty.sh
+++ scripts/kitty.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# install latest
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
+    launch=n
+
+# syslink 
+ls -s -f ~/.local/kitty.app/bin/kitty /usr/bin/kitty
+
 # Create folder
 rm -rf ~/.config/kitty/
 
