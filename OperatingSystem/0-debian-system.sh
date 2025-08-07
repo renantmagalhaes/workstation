b40124f2d7c350ed59634a77a8ba12b28e466e0f
RTM <rtm@insecure.codes>
2025-08-07 10:58:22 +0100

    bump virtualbox version to 7.1

--- OperatingSystem/0-debian-system.sh
+++ OperatingSystem/0-debian-system.sh
@@ -94,7 +94,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y install qemu-system-x86 libvirt-c
 wget -O- -q https://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 sudo apt-get update
-sudo apt-get install -y virtualbox-7.0
+sudo apt-get install -y virtualbox-7.1
 
 # virtualbox user
 sudo usermod -aG vboxusers $USER
