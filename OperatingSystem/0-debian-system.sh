37845110f6053437b2f51341b2663af6192f6d06
RTM <rtm@insecure.codes>
2025-08-07 10:54:29 +0100

    virtualbox new key file location

--- OperatingSystem/0-debian-system.sh
+++ OperatingSystem/0-debian-system.sh
@@ -91,7 +91,7 @@ bash ./scripts/latest-node.sh
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install qemu-system-x86 libvirt-clients libvirt-daemon libvirt-daemon-system virtinst virt-manager bridge-utils ovmf
 
 # Virtualbox
-wget -O- -q https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg
+wget -O- -q https://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 sudo apt-get update
 sudo apt-get install -y virtualbox-7.0
