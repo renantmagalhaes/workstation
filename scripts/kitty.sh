ce42583e0198614fe5d4755a06a2d43a4f11e64d
RTM <rtm@insecure.codes>
2025-08-15 12:45:51 +0100

    fix syslink

--- scripts/kitty.sh
+++ scripts/kitty.sh
@@ -2,10 +2,10 @@
 
 # install latest
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
-    launch=n
+	launch=n
 
-# syslink 
-ls -s -f ~/.local/kitty.app/bin/kitty /usr/bin/kitty
+# syslink
+sudo ln -s -f ~/.local/kitty.app/bin/kitty /usr/bin/kitty
 
 # Create folder
 rm -rf ~/.config/kitty/
