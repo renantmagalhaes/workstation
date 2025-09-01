3613aa842cfe0638469d5f652790529eb5d393ca
RTM <rtm@insecure.codes>
2025-09-01 17:20:47 +0100

    syslink setup

--- dotfiles/bspwm/kde/syslinks.sh
+++ dotfiles/bspwm/kde/syslinks.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #BSPWM for kde
-#ln -s -f $PWD/bspwmrc ~/.config/bspwm/bspwmrc
+rm -rf ~/.config/bspwm
+mkdir -p ~/.config/bspwm
+ln -s -f $PWD/bspwmrc ~/.config/bspwm/bspwmrc
 
 #sxhkdrc for kde
+rm -rf ~/.config/sxhkd
+mkdir -p ~/.config/sxhkd
 ln -s -f $PWD/sxhkdrc ~/.config/sxhkd/sxhkdrc
