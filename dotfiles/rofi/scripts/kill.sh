30fd10676bb6164cd123b4959fed15fdd3f4b3bb
RTM <rtm@insecure.codes>
2026-03-29 13:24:58 +0100

    icons on kill

--- dotfiles/rofi/scripts/kill.sh
+++ dotfiles/rofi/scripts/kill.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 dir="$HOME/.config/rofi/scripts/launchers/type-1"
 theme='style-7'
 
-# List all running processes by their name (excluding duplicates)
-PROCESS_LIST=$(ps -e -o comm= | sort -u)
+# Get list of running processes for the current user, excluding common system/script names
+PROCESS_LIST=$(ps -u "$USER" -o comm= | grep -vE "kill.sh|ps|grep|sort|sed|awk|sh|bash" | sort -u)
+
+# Prepare list for Rofi
+# We just pass the name as the icon name. Rofi will find it if it exists in the current icon theme.
+# This is O(n) and very fast compared to searching desktop files.
+ROFI_INPUT=""
+while read -r comm; do
+    [ -z "$comm" ] && continue
+    ROFI_INPUT+="${comm}\0icon\x1f${comm}\n"
+done <<< "$PROCESS_LIST"
 
 # Show the process list in Rofi for selection
-SELECTED_PROCESS=$(echo "$PROCESS_LIST" | rofi -dmenu -p "kill" -i -theme ${dir}/${theme}.rasi)
+SELECTED_PROCESS=$(echo -ne "$ROFI_INPUT" | rofi -dmenu -p "kill" -i -show-icons -theme ${dir}/${theme}.rasi)
 
 # If a process is selected, kill all its instances
 if [ -n "$SELECTED_PROCESS" ]; then
-	pkill "$SELECTED_PROCESS"
+    pkill -u "$USER" "$SELECTED_PROCESS"
 fi
