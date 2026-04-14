6fcb16f60454bc7d25239f4f99d0267ee2477ffb
RTM <rtm@insecure.codes>
2026-04-14 20:03:22 +0100

    sync

--- dotfiles/nixos/hardware-configuration.nix
+++ dotfiles/nixos/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
