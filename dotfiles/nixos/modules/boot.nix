68e16dd8626597b9dfbe31f26bf3cc85d3370f2e
RTM <rtm@insecure.codes>
2026-04-14 19:00:09 +0100

    sync

--- dotfiles/nixos/modules/boot.nix
+++ dotfiles/nixos/modules/boot.nix
@@ -4,12 +4,12 @@
   # Agnostic GRUB Configuration
   boot.loader = {
     efi = {
-      canTouchEfiVariables = false;
+      canTouchEfiVariables = lib.mkDefault false;
       efiSysMountPoint = "/boot"; # Standard mount point
     };
     grub = {
       enable = true;
-      efiSupport = true;
+      efiSupport = lib.mkDefault false;
       # "nodev" works for UEFI only. For Legacy BIOS, we need a device.
       # We use mkDefault so it's easy to override in host-specific configs.
       device = lib.mkDefault "/dev/sda"; 
@@ -18,7 +18,7 @@
       # device = "nodev"; 
 
       useOSProber = true;
-      efiInstallAsRemovable = true; # Helps with some picky BIOS/UEFI dual-boot setups
+      efiInstallAsRemovable = lib.mkDefault false; # Only for UEFI
     };
   };
 
