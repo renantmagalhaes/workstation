99e7e14b6645c63f7721b99bdf631f27578c33cd
RTM <rtm@insecure.codes>
2026-04-15 12:05:21 +0200

    sync

--- dotfiles/nixos/home-manager/dotfiles.nix
+++ dotfiles/nixos/home-manager/dotfiles.nix
@@ -2,9 +2,10 @@
 
 {
   home.file = {
-    # Symlink the Oh My Posh theme from the dotfiles *flake input*
-    ".config/omp/oh-my-posh-minimal.yaml" = {
-      source = "${inputs.dotfiles}/zsh/omp/oh-my-posh-minimal.yaml";
+    # Oh My Posh themes — symlink the whole folder so all variants are available
+    ".config/omp" = {
+      source = "${inputs.dotfiles}/zsh/omp";
+      recursive = true;
     };
 
     # Groundwork for Polybar, JGMenu, etc.
