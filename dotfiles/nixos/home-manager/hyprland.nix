a21e0877c70db48400119b643830148b2bd2cce7
RTM <rtm@insecure.codes>
2026-04-16 22:54:44 +0200

    sync

--- dotfiles/nixos/home-manager/hyprland.nix
+++ dotfiles/nixos/home-manager/hyprland.nix
@@ -50,6 +50,45 @@ in
     gtk4.theme = config.gtk.theme;
   };
 
+  # Rofi desktop entries (replaces the imperative creation in scripts/rofi.sh)
+  xdg.desktopEntries = {
+    kill-process = {
+      name = "Kill Process";
+      exec = "${home}/.config/rofi/scripts/kill.sh";
+      icon = "utilities-terminal";
+      terminal = false;
+      categories = [ "System" ];
+    };
+    power-menu = {
+      name = "Power Menu";
+      exec = "${home}/.config/rofi/scripts/power-menu.sh";
+      icon = "system-shutdown";
+      terminal = false;
+      categories = [ "System" ];
+    };
+    power-menu-shutdown = {
+      name = "Shutdown";
+      exec = "${home}/.config/rofi/scripts/rofi-shutdown.sh";
+      icon = "system-shutdown";
+      terminal = false;
+      categories = [ "System" ];
+    };
+    power-menu-restart = {
+      name = "Restart";
+      exec = "${home}/.config/rofi/scripts/rofi-restart.sh";
+      icon = "system-reboot";
+      terminal = false;
+      categories = [ "System" ];
+    };
+    power-menu-logout = {
+      name = "Logout";
+      exec = "${home}/.config/rofi/scripts/rofi-logout.sh";
+      icon = "system-log-out";
+      terminal = false;
+      categories = [ "System" ];
+    };
+  };
+
   # QS-Launcher Bootstrap
   # Handles the git clone described in the original install script
   home.activation.setupQSLauncher = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
