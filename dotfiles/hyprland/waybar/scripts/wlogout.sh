cb4dd9c2a6e4fbeb0f6c593886af9ccb8da61577
RTM <rtm@insecure.codes>
2026-04-18 00:06:17 +0100

    sync

--- dotfiles/hyprland/waybar/scripts/wlogout.sh
+++ dotfiles/hyprland/waybar/scripts/wlogout.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Check if wlogout is already running
-if pgrep -f "wlogout" > /dev/null; then
-    pkill -f "wlogout"
-    exit 0
-fi
+# if pgrep -f "wlogout" > /dev/null; then
+#     pkill -f "wlogout"
+#     exit 0
+# fi
 
 wlogout -C $HOME/.config/waybar/extra/wlogout/nova.css -l $HOME/.config/waybar/extra/wlogout/layout --protocol layer-shell -b 4 -T 200 -B 200 &
