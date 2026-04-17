5ab7ac102ade6cf568be6d63d88526c6d3c3b6fe
RTM <rtm@insecure.codes>
2026-04-18 01:04:15 +0200

    sync

--- dotfiles/hyprland/waybar/scripts/wlogout.sh
+++ dotfiles/hyprland/waybar/scripts/wlogout.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-A_1080=250
-B_1080=250
 
 # Check if wlogout is already running
 if pgrep -f "wlogout" > /dev/null; then
@@ -8,7 +6,4 @@ if pgrep -f "wlogout" > /dev/null; then
     exit 0
 fi
 
-# Detect monitor resolution and scaling factor
-resolution=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .height / .scale' | awk -F'.' '{print $1}')
-hypr_scale=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .scale')
 wlogout -C $HOME/.config/waybar/extra/wlogout/nova.css -l $HOME/.config/waybar/extra/wlogout/layout --protocol layer-shell -b 4 -T 200 -B 200 &
