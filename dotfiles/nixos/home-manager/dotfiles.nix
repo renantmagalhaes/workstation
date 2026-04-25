94db3be3d14f241fe932c568d71dedc8166a7bad
RTM <rtm@insecure.codes>
2026-04-25 15:34:23 +0100

    sync

--- dotfiles/nixos/home-manager/dotfiles.nix
+++ dotfiles/nixos/home-manager/dotfiles.nix
@@ -7,8 +7,6 @@ let
 in
 {
   # Ensure ~/.dotfiles is a live symlink to the actual git repo, not a nix-store copy.
-  # NOTE: Initial bootstrapping MUST be done by install.sh because flake.nix
-  # depends on ~/.dotfiles existing at evaluation time (chicken-and-egg problem).
   home.activation.linkDotfiles = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
     run ln -sfn "${home}/GIT-REPOS/workstation/dotfiles" "$HOME/.dotfiles"
   '';
@@ -16,11 +14,14 @@ in
   home.file = {
     ".config/alacritty".source  = link "alacritty";
     ".config/omp".source        = link "zsh/omp";
+    
+    # KDE / Plasma configuration
+    ".config/kglobalshortcutsrc".source = link "kde/shortcuts.kksrc";
+    ".config/kvantum".source            = link "kde/kvantum";
+    
     ".config/fastfetch".source  = link "fastfetch";
     ".config/guake".source      = link "guake";
     ".config/lsd/config.yaml".source = link "zsh/lsd-config.yaml";
     ".p10k.zsh".source          = link "zsh/p10k.zsh";
-    # starship.toml not yet in repo — add dotfiles/zsh/starship/starship.toml to enable
-    # ".config/starship.toml".source = link "zsh/starship/starship.toml";
   };
 }
