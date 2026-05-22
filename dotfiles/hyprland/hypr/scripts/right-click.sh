4b7c2048aa8455d53c8ab48bf87d79231a1b5bb4
RTM <rtm@insecure.codes>
2026-05-22 12:42:17 +0100

    jgmenu dotfiles location

--- dotfiles/hyprland/hypr/scripts/right-click.sh
+++ dotfiles/hyprland/hypr/scripts/right-click.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Fast path: Hyprland says "Invalid" when there is no active window
 if hyprctl activewindow | grep -q '^Invalid'; then
-  exec jgmenu_run
+  exec jgmenu --config-file=~/.dotfiles/hyprland/hypr/jgmenu/jgmenurc --at-pointer
 fi
