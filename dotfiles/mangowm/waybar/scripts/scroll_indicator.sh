669b959b7a609345fec1ac6b31a3366174ede7b7
RTM <rtm@insecure.codes>
2026-05-30 16:59:52 +0100

    scroll indicator edge case

--- dotfiles/mangowm/waybar/scripts/scroll_indicator.sh
+++ dotfiles/mangowm/waybar/scripts/scroll_indicator.sh
@@ -3,17 +3,60 @@
 action=$1
 
 check_scroll() {
-    mmsg -g | awk '
-    $2 == "selmon" && $3 == "1" { selmon = $1 }
-    $2 == "layout" { layout[$1] = $3 }
-    $2 == "tag" && $4 == "1" { clients[$1] = $5 }
-    END {
-        if (selmon && layout[selmon] == "S" && clients[selmon] >= 3) {
-            exit 0
+    eval "$(mmsg -g | awk '
+      $2 == "selmon" && $3 == "1" { active_mon = $1 }
+      active_mon && $1 == active_mon {
+        if ($2 == "tag") {
+          if ($4 % 2 == 1) {
+            active_tag_clients += $5
+          }
         }
-        exit 1
-    }
-    '
+        if ($2 == "width") {
+          client_width = $3
+        }
+        if ($2 == "layout") {
+          layout_symbol = $3
+        }
+      }
+      END {
+        if (active_mon) {
+          print "active_monitor=\"" active_mon "\""
+          print "active_tag_clients=" (active_tag_clients ? active_tag_clients : 0)
+          print "client_width=" (client_width ? client_width : 0)
+          print "layout_symbol=\"" layout_symbol "\""
+        }
+      }
+    ')"
+
+    if [ -z "$active_monitor" ] || [ "$layout_symbol" != "S" ]; then
+        return 1
+    fi
+
+    if [ "${active_tag_clients:-0}" -ge 3 ]; then
+        return 0
+    fi
+
+    if [ "${active_tag_clients:-0}" -eq 2 ] && [ -n "$client_width" ]; then
+        mon_width=$(wlr-randr | awk -v mon="$active_monitor" '
+          $0 ~ "^" mon { flag = 1; next }
+          /^[A-Z]/ { flag = 0 }
+          flag && /^[ ]+[0-9]+x[0-9]+ px/ && /\(current\)/ {
+            gsub(/px,/, "", $1)
+            split($1, a, "x")
+            print a[1]
+            exit
+          }
+        ')
+
+        if [ -n "$mon_width" ]; then
+            width_diff=$((mon_width - client_width))
+            if [ "$width_diff" -le 40 ]; then
+                return 0
+            fi
+        fi
+    fi
+
+    return 1
 }
 
 if [ "$action" = "left" ]; then
